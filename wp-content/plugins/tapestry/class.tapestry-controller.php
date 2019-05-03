4eec40b28dee6f4866807ce1d92526c0f7f4b68b
Andrew Bui <andrewbui130@hotmail.com>
2019-05-03 15:32:35 -0700

    Update multiple child nodes

--- wp-content/plugins/tapestry/class.tapestry-controller.php
+++ wp-content/plugins/tapestry/class.tapestry-controller.php
@@ -2,25 +2,29 @@
 class TapestryController {
     public function updateTapestryPost($post, $postId = null) {
         if (is_null($postId)) $postId = $this->insertPost($post);
+        $this->updateNodes($post['nodes'], $postId);
+
         $post['nodes'] = $this->getNodeIds($post['nodes']);
-        return $this->updateTapestry($post, $postId);
+        $this->updateTapestry($post, $postId);
     }
 
-    public function updateTapestryNode($node, $postId = null) {
+    public function updateTapestryNodes($nodes, $postId = null) {
         if (is_null($postId)) throw new Exception('postId is invalid');
-        return $this->updateNode($node, $postId);
+        $this->updateNodes($nodes, $postId);
     }
 
     public function getTapestryPost($postId) {
-        return $this->getTapestry($postId);
+        $this->getTapestry($postId);
     }
 
     private function updateTapestry($post, $postId) {
-        return update_post_meta($postId, 'tapestry', $post);
+        update_post_meta($postId, 'tapestry', $post);
     }
 
-    private function updateNode($node, $postId) {
-        return update_post_meta($postId, 'node'.$node['id'], $node);
+    private function updateNodes($nodes, $postId) {
+        foreach ($nodes as $node) {
+            update_post_meta($postId, 'node_'.$node['id'], $node);
+        }
     }
     
     private function getTapestry($postId) {
