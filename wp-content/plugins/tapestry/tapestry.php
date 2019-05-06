f08da887e8bb2e3bf1f485910ace699a2053b021
Andrew Bui <andrewbui130@hotmail.com>
2019-05-06 15:17:35 -0700

    Create tapestry plugin

---
+++ wp-content/plugins/tapestry/tapestry.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Tapestry
+ * Description: Custom post type - Tapestry
+ * Version: 1.0
+ * Author: Andrew Bui
+ * Author URI: https://www.andrewbui.ca
+ */
+ /**
+ * Register Tapestry type post on initialization
+ */
