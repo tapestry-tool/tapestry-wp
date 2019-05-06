b26a66907cfb7f570db537412d695ffea78d9bc4
Andrew Bui <andrewbui130@hotmail.com>
2019-05-06 10:29:06 -0700

    Revert "Fix object pointer"
    This reverts commit aed03bf61adce2981c7717215071573687bf5836.

--- wp-content/plugins/tapestry/class.tapestry-controller.php
+++ wp-content/plugins/tapestry/class.tapestry-controller.php
@@ -13,8 +13,9 @@ class TapestryController {
      */
     public function updateTapestryPost($post, $postId = null) {
         if (is_null($postId)) $postId = $this->insertPost($post);
-        $this->updateNodes($post->nodes, $postId);
-        $post->nodes = $this->getNodeIds($post->nodes);
+        $this->updateNodes($post['nodes'], $postId);
+
+        $post['nodes'] = $this->getNodeIds($post['nodes']);
         $this->updateTapestry($post, $postId);
     }
 
@@ -46,21 +47,22 @@ class TapestryController {
 
     private function updateNodes($nodes, $postId) {
         foreach ($nodes as $node) 
-            update_post_meta($postId, 'node_'.$node->id, $node);
+            update_post_meta($postId, 'node_'.$node['id'], $node);
     }
     
     private function getTapestry($postId) {
         $post = get_post_meta($postId, 'tapestry', true);
         $nodes = array_map(function($nodeId) use ($postId) {
             return get_post_meta($postId, 'node_'.$nodeId, true);
-        }, $post->nodes);
-        $post->nodes = $nodes;
+        }, $post['nodes']);
+
+        $post['nodes'] = $nodes;
         return $post;
     }
 
     private function getNodeIds($nodes) {
         return array_map(function($node) {
-            return $node->id;
+            return $node['id'];
         }, $nodes);
     }
 
@@ -69,7 +71,7 @@ class TapestryController {
             'post_type' => 'tapestry',
             'post_status' => 'publish',
             'post_content' => '',
-            'post_title' => $post->settings->tapestrySlug
+            'post_title' => $post['settings']['tapestrySlug']
         ));
     }
 }
