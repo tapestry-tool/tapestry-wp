ff751facb5d8da752ba75247acc1dea0ae690984
Andrew Bui <andrewbui130@hotmail.com>
2019-05-08 14:38:22 -0700

    Remove hardcoded string

--- wp-content/plugins/tapestry/templates/single-tapestry.php
+++ wp-content/plugins/tapestry/templates/single-tapestry.php
@@ -24,19 +24,19 @@ get_header(); ?>
             <div id="tapestry"></div>
 
             <link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" rel="stylesheet" />
-			<link href="<?php echo get_site_url(); ?>/wp-content/plugins/tapestry/templates/tapestry-d3/tapestry.css" rel="stylesheet" />
-            <link href="<?php echo get_site_url(); ?>/wp-content/plugins/tapestry/templates/tapestry-d3/libs/jquery-ui.min.css" rel="stylesheet" />
+			<link href="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/tapestry.css" rel="stylesheet" />
+            <link href="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/jquery-ui.min.css" rel="stylesheet" />
 
-            <script src="<?php echo get_site_url(); ?>/wp-content/plugins/tapestry/templates/tapestry-d3/libs/jquery.min.js" type="application/javascript"></script>
-            <script src="<?php echo get_site_url(); ?>/wp-content/plugins/tapestry/templates/tapestry-d3/libs/jquery-ui.min.js" type="application/javascript"></script>
-            <script src="<?php echo get_site_url(); ?>/wp-content/plugins/tapestry/templates/tapestry-d3/libs/jscookie.js" type="application/javascript"></script>
-            <script src="<?php echo get_site_url(); ?>/wp-content/plugins/tapestry/templates/tapestry-d3/libs/d3.v5.min.js" type="application/javascript"></script>
-			<script src="<?php echo get_site_url(); ?>/wp-content/plugins/tapestry/templates/tapestry-d3/libs/h5p-resizer.min.js" charset="UTF-8"></script>
+            <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/jquery.min.js" type="application/javascript"></script>
+            <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/jquery-ui.min.js" type="application/javascript"></script>
+            <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/jscookie.js" type="application/javascript"></script>
+            <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/d3.v5.min.js" type="application/javascript"></script>
+			<script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/h5p-resizer.min.js" charset="UTF-8"></script>
 
             <script>
-            	var jsonUrl = "<?php echo get_site_url(); ?>/wp-content/plugins/tapestry/templates/tapestry-d3/tapestry.json";
+            	var jsonUrl = "<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/tapestry.json";
             </script>
-            <script src="<?php echo get_site_url(); ?>/wp-content/plugins/tapestry/templates/tapestry-d3/tapestry.js"></script>
+            <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/tapestry.js"></script>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
