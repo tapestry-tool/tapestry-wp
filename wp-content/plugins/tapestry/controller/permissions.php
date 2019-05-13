37fc0f763ee501f497d0504901a42c4e7ad63a64
Andrew Bui <andrewbui130@hotmail.com>
2019-05-13 12:52:46 -0700

    Fix a condition

--- wp-content/plugins/tapestry/controller/permissions.php
+++ wp-content/plugins/tapestry/controller/permissions.php
@@ -6,14 +6,14 @@
 
 function postTapestryPermissions($request) {
     $data = json_decode($request->get_body());
-    if ($data->postId)
+    if (isset($data->postId))
         return current_user_can('edit', $data->postId);
     return current_user_can('publish_posts');
 }
 
 function postTapestryNodePermissions($request) {
     $data = json_decode($request->get_body());
-    if ($data->postId)
+    if (isset($data->postId))
         return current_user_can('edit', $data->postId);
     return current_user_can('publish_posts');
 }
