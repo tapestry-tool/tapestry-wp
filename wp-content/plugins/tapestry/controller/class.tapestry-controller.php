1b3270d3da0ff07fd71c392c3ad038a430ae21e9
Andrew Bui <andrewbui130@hotmail.com>
2019-05-14 15:01:11 -0700

    Add endpoint for changing Tapestry settings

--- wp-content/plugins/tapestry/controller/class.tapestry-controller.php
+++ wp-content/plugins/tapestry/controller/class.tapestry-controller.php
@@ -3,4 +3,18 @@
  * Add/update/retrieve Tapestry post and its child nodes
  * 
  */
-class TapestryController {}
+class TapestryController {
+    /**
+     * Update Tapestry settings
+     * 
+     * @param type @settings The updated Tapestry settings
+     * @param type @postId The postId of the Tapestry
+     * @return type updated Tapestry settings
+     */
+    public function updateTapestrySettings($settings, $postId) {
+        // 1. get Tapestry with postId
+        // 2. update settings
+        // 3. return the updated settings
+        return $settings;
+    }
+}
--- wp-content/plugins/tapestry/endpoints.php
+++ wp-content/plugins/tapestry/endpoints.php
@@ -3,3 +3,17 @@
  * Tapestry Endpoints
  *
  */
+
+add_action('rest_api_init', function () {
+    register_rest_route('tapestry-tool/v1', '/tapestry-settings', array(
+        'methods' => 'POST',
+        'callback' => 'updateTapestrySettings'
+        // TODO: Add permissions here later, when saving tapestry PR is merged
+    ));
+});
+
+function updateTapestrySettings($request) {
+    $data = json_decode($request->get_body());
+    $tapestryController = new TapestryController;
+    return $tapestryController->updateTapestrySettings($data, $data->postId);
+}
