4e2eff43eed0f42f619a17e840e9fa84513762f6
kevinthewebguy <kevinwong_895@hotmail.com>
2019-05-14 19:18:36 -0700

    Changing endpoints to not receive userid, save node progress one node at a time. Adding tapestry_ prefix to metakey

--- wp-content/plugins/tapestry/controller/class.user-controller.php
+++ wp-content/plugins/tapestry/controller/class.user-controller.php
@@ -7,39 +7,52 @@ class TapestryUserController {
 
     /** 
      * Update User's video progress for a tapestry post
-     * @param type @userId the user's id
      * @param type @postId the post's ID
-     * @param type @progressData json data for progress of each node
+     * @param type @nodeId the current node being viewed
+     * @param type @progressValue is how much the video was viewed, value should be between >= 0 and <= 1
     */
-    public function updateProgress($userId, $postId, $progressData) {
-        $this->checkUserAndPostId($userId, $postId);  
+    public function updateProgress($postId, $nodeId, $progressValue) {
+        $userId = apply_filters('determine_current_user', false);
+        $this->checkUserAndPostId($userId, $postId);
 
-        if ($this->isJson($progressData)) {
-            $progressData = json_decode($progressData);
-        } else {
-            throw new Exception('Invalid json');
+        if ($progressValue !== NULL) {
+            $progressValue = json_decode($progressValue);
         }
 
-        $this->updateUserProgress($userId, $postId, $progressData);
+        // Value should be between 0 and 1
+        if ($progressValue < 0 || $progressValue > 1) {
+            throw new Exception('Invalid progress value');
+        }
+
+        $this->updateUserProgress($userId, $postId, $nodeId, $progressValue);
     }
 
     /** 
      * Get User's video progress for a tapestry post
-     * @param type @userId the user's id
      * @param type @postId the post's ID
+     * @param type @nodeIdArr is a list of ids currently in the tapestry
     */
-    public function getProgress($userId, $postId) {
+    public function getProgress($postId, $nodeIdArr) {
+        $userId = apply_filters('determine_current_user', false);
         $this->isPostValid($postId);
-        $this->checkUserAndPostId($userId, $postId);  
-        return $this->getUserProgress($userId, $postId);
+        $this->checkUserAndPostId($userId, $postId);
+
+        if ($this->isJson($nodeIdArr)) {
+            $nodeIdArr = json_decode($nodeIdArr);
+        } else {
+            throw new Exception('Invalid json');
+        }
+
+        return $this->getUserProgress($userId, $postId, $nodeIdArr);
     }
 
     /** 
      * Update User's h5p video setting for a tapestry post
-     * @param type @userId the user's id
      * @param type @postId the post's ID
+     * @param type @h5pSettingsData stores volume, playbackRate, quality of h5p video
     */
-    public function updateH5PSetting($userId, $postId, $h5pSettingsData) {
+    public function updateH5PSetting($postId, $h5pSettingsData) {
+        $userId = apply_filters('determine_current_user', false);
         $this->checkUserAndPostId($userId, $postId);  
 
         if ($this->isJson($h5pSettingsData)) {
@@ -53,41 +66,48 @@ class TapestryUserController {
 
     /** 
      * Get User's h5p video setting for a tapestry post
-     * @param type @userId the user's id
-     * @param type @postId the post's ID
+     * @param type @postId the post's Id
     */
-    public function getH5PSetting($userId, $postId) {
+    public function getH5PSetting($postId) {
+        $userId = apply_filters('determine_current_user', false);
         $this->isPostValid($postId);
         $this->checkUserAndPostId($userId, $postId);  
         return $this->getUserH5PSetting($userId, $postId);
     }
 
-    private function updateUserProgress($userId, $postId, $progressData) {
-        update_user_meta($userId, 'progress_data_' . $postId, $progressData);
+    private function updateUserProgress($userId, $postId, $nodeId, $progressValue) {
+        update_user_meta($userId, 'tapestry_' . $postId . '_progress_node_' . $nodeId, $progressValue);
     }
 
-    private function getUserProgress($userId, $postId) {
-        $progress = get_user_meta($userId, 'progress_data_' . $postId, true);
-        return $progress;
+    private function getUserProgress($userId, $postId, $nodeIdArr) {
+        $progress = new stdClass();
+
+        // Build json object for frontend e.g. {0: 0.1, 1: 0.2} where 0 and 1 are the node IDs
+        foreach ($nodeIdArr as $nodeId) {
+            $progress_value = get_user_meta($userId, 'tapestry_' . $postId . '_progress_node_' . $nodeId, true);
+            if ($progress_value !== NULL) {
+                $progress->$nodeId = $progress_value;
+            } else {
+                $progress->$nodeId = 0.0;
+            }
+        }
+
+        return json_encode($progress);
     }
 
     private function updateUserH5PSetting($userId, $postId, $h5pSettingsData) {
-        update_user_meta($userId, 'h5p_setting_' . $postId, $h5pSettingsData);
+        update_user_meta($userId, 'tapestry_h5p_setting_' . $postId, $h5pSettingsData);
     }
 
     private function getUserH5PSetting($userId, $postId) {
-        $settings = get_user_meta($userId, 'h5p_setting_' . $postId, true);
-        return $settings;
+        $settings = get_user_meta($userId, 'tapestry_h5p_setting_' . $postId, true);
+        return json_encode($settings);
     }
 
     // Helpers
     private function checkUserAndPostId( $userId, $postId) {
+        // User not logged in or userId is not set. Comment out when testing on Postman
         if (!isset($userId)) {
-            throw new Exception('userId is invalid');
-        }
-
-        // Not the current user. Comment out when testing the endpoints on Postman
-        if ($userId != apply_filters('determine_current_user', false)) {
             throw new Exception('userId is invalid');
         }
 
@@ -104,7 +124,7 @@ class TapestryUserController {
         }
 
         // Post type is correct
-        if (!get_post_type($postId) == "tapestry") {
+        if (get_post_type($postId) != "tapestry") {
             throw new Exception('post type is invalid');
         }
     }
--- wp-content/plugins/tapestry/endpoints.php
+++ wp-content/plugins/tapestry/endpoints.php
@@ -17,28 +17,28 @@ add_action( 'rest_api_init', function () {
 });
 
 // Get user progress on a tapestry page by user id and post id. Will need to pass these as query parameters
-// Example: http://localhost:8888/tapestry-wp/wp-json/tapestry-tool/v1/users/progress?user_id=1&post_id=42
+// Example: http://localhost:8888/tapestry-wp/wp-json/tapestry-tool/v1/users/progress?post_id=44&node_id_arr=[1,2,3,4,5,6]
 function tapestry_get_user_progress_by_post_id($data) {
-    $userId = $data['user_id'];
     $postId = $data['post_id'];
+    $nodeIdArr = $data['node_id_arr'];
     $userController = new TapestryUserController;
-    return $userController->getProgress($userId, $postId);
+    return $userController->getProgress($postId, $nodeIdArr);
 }
 
 add_action( 'rest_api_init', function () {
     register_rest_route( 'tapestry-tool/v1', '/users/progress/', array(
       'methods' => 'POST',
-      'callback' => 'tapestry_update_user_progress_by_post_id',
+      'callback' => 'tapestry_update_user_progress_by_node_id',
     ) );
 });
 
-// Example: http://localhost:8888/tapestry-wp/wp-json/tapestry-tool/v1/users/progress?user_id=1&post_id=44&json={"32":0.5,"33":0,"34":0,"35":0,"36":0,"37":0,"38":0,"39":0,"40":0,"41":0}
-function tapestry_update_user_progress_by_post_id($data) {
+// Example: http://localhost:8888/tapestry-wp/wp-json/tapestry-tool/v1/users/progress?post_id=44&node_id=1&progress_value=0.2
+function tapestry_update_user_progress_by_node_id($data) {
     $userController = new TapestryUserController;
-    $userId = $data['user_id'];
     $postId = $data['post_id'];
-    $json = $data['json'];
-    $userController->updateProgress($userId, $postId, $json);
+    $nodeId = $data['node_id'];
+    $progressValue = $data['progress_value'];
+    $userController->updateProgress($postId, $nodeId, $progressValue);
 }
 
 // Get user's h5p video settings
@@ -50,12 +50,11 @@ add_action( 'rest_api_init', function () {
 });
 
 // Get user h5p video setting on a tapestry page by user id and post id. Will need to pass these as query parameters
-// Example: http://localhost:8888/tapestry-wp/wp-json/tapestry-tool/v1/users/h5psettings?user_id=1&post_id=42
+// Example: http://localhost:8888/tapestry-wp/wp-json/tapestry-tool/v1/users/h5psettings?post_id=42
 function tapestry_get_user_h5p_settings_by_post_id($data) {
-    $userId = $data['user_id'];
     $postId = $data['post_id'];
     $userController = new TapestryUserController;
-    return $userController->getH5PSetting($userId, $postId);
+    return $userController->getH5PSetting($postId);
 }
 
 add_action( 'rest_api_init', function () {
@@ -65,11 +64,10 @@ add_action( 'rest_api_init', function () {
     ) );
 });
 
-// Example: http://localhost:8888/tapestry-wp/wp-json/tapestry-tool/v1/users/h5psettings?user_id=1&post_id=44&json={"volume":100,"muted":false,"caption":null,"quality":"q1","playbackRate":0.5,"time":11.934346}
+// Example: http://localhost:8888/tapestry-wp/wp-json/tapestry-tool/v1/users/h5psettings?post_id=44&json={"volume":100,"muted":false,"caption":null,"quality":"q1","playbackRate":0.5,"time":11.934346}
 function tapestry_update_user_h5p_settings_by_post_id($data) {
     $userController = new TapestryUserController;
-    $userId = $data['user_id'];
     $postId = $data['post_id'];
     $json = $data['json'];
-    $userController->updateH5PSetting($userId, $postId, $json);
+    $userController->updateH5PSetting($postId, $json);
 }
