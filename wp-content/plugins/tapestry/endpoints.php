9c44cecef63a9d10e6af902358c2ccc1c82cd03d
Andrew Bui <andrewbui130@hotmail.com>
2019-05-15 11:07:02 -0700

    Update endpoint to be a PUT endpoint

--- wp-content/plugins/tapestry/endpoints.php
+++ wp-content/plugins/tapestry/endpoints.php
@@ -3,17 +3,19 @@
  * Tapestry Endpoints
  *
  */
+require __DIR__ . '/controller/class.tapestry-controller.php';
 
 add_action('rest_api_init', function () {
-    register_rest_route('tapestry-tool/v1', '/tapestry-settings', array(
-        'methods' => 'POST',
+    register_rest_route('tapestry-tool/v1', '/tapestries/(?P<id>[\d]+)/settings', array(
+        'methods' => 'PUT',
         'callback' => 'updateTapestrySettings'
         // TODO: Add permissions here later, when saving tapestry PR is merged
     ));
 });
 
 function updateTapestrySettings($request) {
+    $postId = $request['id'];
     $data = json_decode($request->get_body());
     $tapestryController = new TapestryController;
-    return $tapestryController->updateTapestrySettings($data, $data->postId);
+    return $tapestryController->updateTapestrySettings($data, $postId);
 }
