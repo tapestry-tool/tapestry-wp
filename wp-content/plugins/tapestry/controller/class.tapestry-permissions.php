a7257f4bbdfafb0269673b1e9f01bf6b090a3c87
Andrew Bui <andrewbui130@hotmail.com>
2019-05-14 17:27:10 -0700

    Make a class permissions

---
+++ wp-content/plugins/tapestry/controller/class.tapestry-permissions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Tapestry Endpoint Permissions
+ *
+ */
+class TapestryPermissions {
+    static function postTapestryPermissions($request) {
+        $data = json_decode($request->get_body());
+        if (isset($data->postId))
+            return current_user_can('edit', $data->postId);
+        return current_user_can('publish_posts');
+    }
+    
+    static function postTapestryNodePermissions($request) {
+        $data = json_decode($request->get_body());
+        if (isset($data->postId))
+            return current_user_can('edit', $data->postId);
+        return current_user_can('publish_posts');
+    }
+}
--- wp-content/plugins/tapestry/endpoints.php
+++ wp-content/plugins/tapestry/endpoints.php
@@ -4,22 +4,14 @@
  *
  */
 
-require __DIR__ . '/controller/permissions.php';
+require __DIR__ . '/controller/class.tapestry-permissions.php';
 require __DIR__ . '/controller/class.tapestry-controller.php';
 
 add_action( 'rest_api_init', function () {
     register_rest_route( 'tapestry-tool/v1', '/tapestries', array(
         'methods' => 'POST',
         'callback' => 'updateTapestry',
-        'permission_callback' => 'postTapestryPermissions'
-    ));
-});
-
-add_action( 'rest_api_init', function () {
-    register_rest_route( 'tapestry-tool/v1', '/tapestry-nodes', array(
-        'methods' => 'POST',
-        'callback' => 'updateTapestryNodes',
-        'permission_callback' => 'postTapestryNodePermissions'
+        'permission_callback' => 'TapestryPermissions::postTapestryPermissions'
     ));
 });
 
@@ -28,6 +20,14 @@ function updateTapestry($request) {
     $tapestryController = new TapestryController;
     return $tapestryController->updateTapestry($data, $data->postId);
 }
+
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'tapestry-tool/v1', '/tapestry-nodes', array(
+        'methods' => 'POST',
+        'callback' => 'updateTapestryNodes',
+        'permission_callback' => 'TapestryPermissions::postTapestryNodePermissions'
+    ));
+});
 
 function updateTapestryNodes($request) {
     $data = json_decode($request->get_body());
