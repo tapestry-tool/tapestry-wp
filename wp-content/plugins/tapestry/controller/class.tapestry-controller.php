f39eb5446a9a59d826e7bb82e8e9ca3cc8f07bd8
Andrew Bui <andrewbui130@hotmail.com>
2019-05-15 14:46:17 -0700

    Implementation for adding node to a tapestry

--- wp-content/plugins/tapestry/controller/class.tapestry-controller.php
+++ wp-content/plugins/tapestry/controller/class.tapestry-controller.php
@@ -3,4 +3,24 @@
  * Add/update/retrieve Tapestry post and its child nodes
  * 
  */
-class TapestryController {}
+class TapestryController {
+    /**	
+     * Update Tapestry post's child nodes	
+     * 	
+     * @param type @nodes An array of child nodes to be updated/added
+     * @param type @postId The post ID to which the nodes belong	
+     * @return WP_Error if postId is invalid	
+     */	
+    public function updateTapestryNodes($nodes, $postId = null) {	
+        // TODO: check if $nodes param is a valid JSON	
+        // TODO: use isValidPostID() utlility function
+        /*
+        if (!isset($postId)) {	
+            return $this->throwsError('INVALID_POST_ID');	
+        }
+        */
+        // TODO: uncomment the line below when saving tapestry PR is merged
+        // $this->updateNodes($nodes, $postId);
+        return $nodes;
+    }
+}
--- wp-content/plugins/tapestry/endpoints.php
+++ wp-content/plugins/tapestry/endpoints.php
@@ -3,3 +3,19 @@
  * Tapestry Endpoints
  *
  */
+require __DIR__ . '/controller/class.tapestry-controller.php';
+add_action( 'rest_api_init', function () {	
+    register_rest_route('tapestry-tool/v1', '/tapestries/(?P<tapestryId>[\d]+)/nodes', array(	
+        'methods' => 'POST',	
+        'callback' => 'updateTapestryNodes',	
+        // TODO: Add permission here
+    ));	
+});
+
+function updateTapestryNodes($request) {
+    $postId = $request['tapestryId'];
+    $data = json_decode($request->get_body());
+    // TODO: json and postId validations should happen here
+    $tapestryController = new TapestryController;	
+    return $tapestryController->updateTapestryNodes($data, $postId);	
+}
