706da7d19c6db940c308ad10f065c0541dfb0026
Andrew Bui <andrewbui130@hotmail.com>
2019-05-15 10:39:53 -0700

    Add brackets for if statement

--- wp-content/plugins/tapestry/controller/class.tapestry-permissions.php
+++ wp-content/plugins/tapestry/controller/class.tapestry-permissions.php
@@ -6,15 +6,17 @@
 class TapestryPermissions {
     static function postTapestryPermissions($request) {
         $data = json_decode($request->get_body());
-        if (isset($data->postId))
+        if (isset($data->postId)) {
             return current_user_can('edit', $data->postId);
+        }
         return current_user_can('publish_posts');
     }
     
     static function postTapestryNodePermissions($request) {
         $data = json_decode($request->get_body());
-        if (isset($data->postId))
+        if (isset($data->postId)) {
             return current_user_can('edit', $data->postId);
+        }
         return current_user_can('publish_posts');
     }
 }
