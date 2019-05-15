378953eb6df1983f856d37177179669a7ef86856
Andrew Bui <andrewbui130@hotmail.com>
2019-05-15 13:51:18 -0700

    Remove updating child nodes

--- wp-content/plugins/tapestry/controller/class.tapestry-controller.php
+++ wp-content/plugins/tapestry/controller/class.tapestry-controller.php
@@ -42,23 +42,6 @@ class TapestryController {
         return $tapestry;
     }
 
-    /**
-     * Update Tapestry post's child nodes
-     * 
-     * @param type @nodes An array of child nodes to be updated
-     * @param type @postId The post ID to which the nodes belong
-     * @return WP_Error if postId is invalid
-     */
-    public function updateTapestryNodes($nodes, $postId = null) {
-        // TODO: check if $nodes param is a valid JSON
-        // TODO: use isValidPostID() utlility function
-        if (!isset($postId)) {
-            return $this->throwsError('INVALID_POST_ID');
-        }
-        $this->updateNodes($nodes, $postId);
-        return $nodes;
-    }
-
     private function updateNodes($nodes, $postId) {
         foreach ($nodes as $node) {
             if (!isset($node->id)) {
--- wp-content/plugins/tapestry/endpoints.php
+++ wp-content/plugins/tapestry/endpoints.php
@@ -20,17 +20,3 @@ function updateTapestry($request) {
     $tapestryController = new TapestryController;
     return $tapestryController->updateTapestry($data, $data->postId);
 }
-
-add_action( 'rest_api_init', function () {
-    register_rest_route( 'tapestry-tool/v1', '/tapestry-nodes', array(
-        'methods' => 'POST',
-        'callback' => 'updateTapestryNodes',
-        'permission_callback' => 'TapestryPermissions::postTapestryNodePermissions'
-    ));
-});
-
-function updateTapestryNodes($request) {
-    $data = json_decode($request->get_body());
-    $tapestryController = new TapestryController;
-    return $tapestryController->updateTapestryNodes($data, $data->postId);
-}
