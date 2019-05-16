531863acab39bd774c8220ae2ae00c5dfe7cda9c
Andrew Bui <andrewbui130@hotmail.com>
2019-05-16 15:07:28 -0700

    Add _ to private methods

--- wp-content/plugins/tapestry/controller/class.tapestry-controller.php
+++ wp-content/plugins/tapestry/controller/class.tapestry-controller.php
@@ -14,15 +14,15 @@ class TapestryController {
         // TODO: uncomment the two lines below for error handling
         // after the PR for saving tapestry is merged.
         /*
-        if (!$this->isValidPostId($postId)) {
+        if (!$this->_isValidPostId($postId)) {
             return $this->throwsError('INVALID_POST_ID');
         }
         */
-        $tapestry = $this->getTapestryById($postId);
+        $tapestry = $this->_getTapestryById($postId);
         return $tapestry;
     }
 
-    private function getTapestryById($postId) {
+    private function _getTapestryById($postId) {
         $tapestry = get_post_meta($postId, 'tapestry', true); 
         $metadatas = array_map(function($nodeId) {
             return get_metadata_by_mid('post', $nodeId);
@@ -30,17 +30,17 @@ class TapestryController {
         $nodeDatas = array_map(function($metadata) {
             $postId = $metadata->meta_value->post_id;
             $nodeData = get_post_meta($postId, 'tapestry_node_data', true);
-            return $this->updateNodeData($nodeData, $metadata);
+            return $this->_updateNodeData($nodeData, $metadata);
         }, $metadatas);
 
         $tapestry->nodes = $nodeDatas;
 
         // TODO: delete the below when being able to create tapestry from scratch
-        $tapestry->links = $this->getNewLinks($tapestry->links, $nodeDatas);
+        $tapestry->links = $this->_getNewLinks($tapestry->links, $nodeDatas);
         return $tapestry;
     }
 
-    private function updateNodeData($nodeData, $metadata) {
+    private function _updateNodeData($nodeData, $metadata) {
         // Update node data here to match its own version
         // This enables the same node to have multiple versions
         $nodeData->id = (int) $metadata->meta_id;
@@ -52,7 +52,7 @@ class TapestryController {
 
     // TODO: Remove this when we can build a tapestry from scratch
     // HACK - create a new links array that works with new IDs
-    private function getNewLinks($oldLinks, $nodes) {
+    private function _getNewLinks($oldLinks, $nodes) {
         $mappings = array(
             1 => $nodes[0]->id,
             2 => $nodes[1]->id,
@@ -76,7 +76,7 @@ class TapestryController {
     }
 
     // TODO: this function could be used as a utility function
-    private function isValidPostId($postId) {
+    private function _isValidPostId($postId) {
         return isset($postId) && get_post_status($postId) != false;
     }
 }
