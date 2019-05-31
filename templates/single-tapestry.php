afdca2215bbb37ada4348bbe288c9be0ffac30be
kevinthewebguy <kevinwong_895@hotmail.com>
2019-05-31 11:40:16 -0700

    Trying to update database with new node

--- templates/single-tapestry.php
+++ templates/single-tapestry.php
@@ -38,7 +38,6 @@ get_header(); ?>
 			<script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/bootstrap.min.js" charset="UTF-8"></script>
 
             <script>
-				var jsonUrl = "<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/tapestry.json";
 				var apiUrl = "<?php echo get_rest_url(null, 'tapestry-tool/v1'); ?>";
 				var tapestryWpUserId = "<?php echo apply_filters('determine_current_user', false);?>";
 				var tapestryWpPostId = "<?php echo get_the_ID();?>";
