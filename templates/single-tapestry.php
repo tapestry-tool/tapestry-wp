dec2644ef14c916e779a35996b969a9e4481dbd7
kevinthewebguy <kevinwong_895@hotmail.com>
2019-06-02 16:54:59 -0700

    Adding d3 link and including bootstrap library

--- templates/single-tapestry.php
+++ templates/single-tapestry.php
@@ -21,17 +21,19 @@ get_header(); ?>
 
 			<?php endwhile; // end of the loop. ?>
 			
-            <div id="tapestry"></div>
-
+			<div id="tapestry"></div>
+			
             <link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" rel="stylesheet" />
 			<link href="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/tapestry.css" rel="stylesheet" />
-            <link href="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/jquery-ui.min.css" rel="stylesheet" />
+			<link href="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/jquery-ui.min.css" rel="stylesheet" />
+			<link href="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/bootstrap.min.css" rel="stylesheet" />
 
             <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/jquery.min.js" type="application/javascript"></script>
             <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/jquery-ui.min.js" type="application/javascript"></script>
             <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/jscookie.js" type="application/javascript"></script>
             <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/d3.v5.min.js" type="application/javascript"></script>
 			<script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/h5p-resizer.min.js" charset="UTF-8"></script>
+			<script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/bootstrap.min.js" charset="UTF-8"></script>
 
             <script>
 				var apiUrl = "<?php echo get_rest_url(null, 'tapestry-tool/v1'); ?>";
