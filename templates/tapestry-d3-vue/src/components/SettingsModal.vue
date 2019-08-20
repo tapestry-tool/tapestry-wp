708a4d4024a61304fecc649bc8d54cef383b5ef0
kevinthewebguy <kevinwong_895@hotmail.com>
2019-08-20 12:46:44 -0700

    initial commit for mgiration to vue for settings modal

---
+++ templates/tapestry-d3-vue/src/components/SettingsModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="settings-modal-container">
+    <!-- Modal -->
+    <div class="modal fade" id="tapestrySettingModal" tabindex="-1" role="dialog" aria-labelledby="tapestrySettingModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="tapestrySettingModalLabel">Tapestry Settings</h4>
+            </div>
+            <div class="modal-body" id="tapestrySettingModalBody">
+                <form class="form-container">
+                    <label>Background Image
+                        <input id="tapestry-background-img-input" name="background-image" type="text" placeholder="Enter image URL" required >
+                    </label>
+                    <input id="tapestry-auto-layout-checkbox" type="checkbox" required>
+                    <label id="tapestry-auto-layout-label">Auto Layout</label>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal" id="tapestry-cancel-edit-setting">Close</button>
+                <button type="button" class="btn btn-primary" id="tapestry-submit-edit-setting">Submit</button>
+            </div>
+        </div>
+        </div>
+    </div>
+  </div>
+</template>
