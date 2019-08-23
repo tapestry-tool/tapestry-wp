ddaf0a99b34f665eec53e8012317206fc1346dee
Aidin Niavarani <aidin@wynnset.com>
2019-08-23 12:39:01 -0700

    Change single-tapestry to instead use the new more OO tapestry (#97)
    * Change single-tapestry to instead use the new more OO tapestry that is instantiated and uses prototypes

--- templates/single-tapestry.php
+++ templates/single-tapestry.php
@@ -16,14 +16,25 @@ function addNonceToScript()
     );
 
     wp_register_script(
-        'wp_api_script',
+        'wp_tapestry_script',
         plugin_dir_url(__FILE__) . 'tapestry-d3/tapestry.js',
         array('jquery'),
         null,
         true
     );
-    wp_localize_script('wp_api_script', 'wpApiSettings', $params);
-    wp_enqueue_script('wp_api_script');
+    wp_localize_script('wp_tapestry_script', 'wpApiSettings', $params);
+    wp_enqueue_script('wp_tapestry_script');
+
+    wp_add_inline_script( 'wp_tapestry_script', "
+        var thisTapestryTool = new tapestryTool({
+            'containerId': 'tapestry',
+            'apiUrl': '". get_rest_url(null, 'tapestry-tool/v1') ."',
+            'wpUserId': '". apply_filters('determine_current_user', false) ."',
+            'wpPostId': '". get_the_ID() ."',
+            'wpIsAdmin': '". current_user_can('administrator') ."',
+            'addNodeModalUrl': '". plugin_dir_url( __FILE__ ) ."modal-add-node.html',
+        });
+    " );
 }
 add_action('wp_enqueue_scripts', 'addNonceToScript');
 
@@ -56,14 +67,6 @@ get_header(); ?>
         <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/d3.v5.min.js" type="application/javascript"></script>
         <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/h5p-resizer.min.js" charset="UTF-8"></script>
         <script src="<?php echo plugin_dir_url(__FILE__) ?>tapestry-d3/libs/bootstrap.min.js" charset="UTF-8"></script>
-
-        <script>
-            var apiUrl = "<?php echo get_rest_url(null, 'tapestry-tool/v1'); ?>";
-            var tapestryWpUserId = "<?php echo apply_filters('determine_current_user', false); ?>";
-            var tapestryWpPostId = "<?php echo get_the_ID(); ?>";
-            var tapestryWpIsAdmin = "<?php echo current_user_can('administrator'); ?>";
-            var addNodeModalUrl = "<?php echo plugin_dir_url( __FILE__ ); ?>modal-add-node.html";
-        </script>
 
         </main><!-- #main -->
     </div><!-- #primary -->
