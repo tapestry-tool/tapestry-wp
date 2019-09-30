096048933a3940e79f826c19f578ae511ae6c05b
Narendras <n.syahrasyad.n@gmail.com>
2019-09-30 11:54:02 -0700

    Updated base branch to tyde

--- templates/vue/src/components/Tyde.vue
+++ templates/vue/src/components/Tyde.vue
@@ -1,16 +1,39 @@
 <template>
   <div id="tyde">
     <Tapestry />
+    <TydeMenu v-if="isMenuOpen" />
   </div>
 </template>
 
 <script>
 import Tapestry from "./Tapestry"
+import TydeMenu from "./TydeMenu"
 
 export default {
   name: 'tyde',
   components: {
-    Tapestry
+    Tapestry,
+    TydeMenu,
+  },
+  mounted() {
+    window.addEventListener("keydown", evt => {
+      if (evt.code === "Escape") {
+        this.toggleMenu()
+      }
+    })
+  },
+  beforeDestroy() {
+    window.removeEventListener("keydown")
+  },
+  data() {
+    return {
+      isMenuOpen: false
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen
+    }
   }
 }
 </script>
---
+++ templates/vue/src/components/TydeMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="tyde-menu">
+    <p>I'm the TYDE menu.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tyde-menu'
+}
+</script>
+
+<style scoped>
+#tyde-menu {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: white;
+  z-index: 10;
+}
+</style>
