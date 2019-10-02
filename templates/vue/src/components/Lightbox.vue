35f2536fea94e8056662abfb79f77754ac432199
Narendras <n.syahrasyad.n@gmail.com>
2019-10-02 15:21:27 -0700

    Added boilerplate lightbox

---
+++ templates/vue/src/components/Lightbox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div id="spotlight-overlay"></div>
+    <div id="spotlight-content">
+      <button id='lightbox-close-wrapper'>
+        <div class='lightbox-close'>
+          <i class='fa fa-times'></i>
+        </div>
+      </button>
+      <div class="media-wrapper">
+        <LightboxMedia />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LightboxMedia from './LightboxMedia'
+
+export default {
+  name: 'lightbox',
+  components: {
+    LightboxMedia,
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ templates/vue/src/components/LightboxMedia.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>I'm a media component</p>
+</template>
+
+<script>
+export default {
+  name: 'lightbox-media'
+}
+</script>
+
+<style scoped>
+
+</style>
