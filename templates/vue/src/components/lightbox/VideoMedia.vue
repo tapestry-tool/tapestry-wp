ca4e39426447c6019eb809a943e5597c87510fa2
Narendras <n.syahrasyad.n@gmail.com>
2019-10-29 16:18:52 -0700

    Added icons to end screen

--- templates/vue/src/components/lightbox/VideoMedia.vue
+++ templates/vue/src/components/lightbox/VideoMedia.vue
@@ -1,20 +1,35 @@
 <template>
-  <div v-if="showEndScreen" style="width: 100%; height: 100%;">
-    <h1>I'm the end screen!</h1>
+  <div class="container">
+    <div
+      :class="[
+        'end-screen',
+        {
+          'end-screen--hide': !showEndScreen,
+        },
+      ]"
+    >
+      <button class="end-screen-button">
+        <i class="fas fa-redo fa-4x"></i>
+        <p class="end-screen-button-text">Rewatch</p>
+      </button>
+      <button class="end-screen-button">
+        <i class="far fa-times-circle fa-4x"></i>
+        <p class="end-screen-button-text">Close</p>
+      </button>
+    </div>
+    <video
+      ref="video"
+      class="video"
+      controls
+      autoplay
+      @loadedmetadata="handleLoad"
+      @play="handlePlay"
+      @pause="handlePause"
+      @timeupdate="updateVideoProgress"
+    >
+      <source id="video-source" :src="node.typeData.mediaURL" type="video/mp4" />
+    </video>
   </div>
-  <video
-    v-else
-    ref="video"
-    class="video"
-    controls
-    autoplay
-    @loadedmetadata="handleLoad"
-    @play="handlePlay"
-    @pause="handlePause"
-    @timeupdate="updateVideoProgress"
-  >
-    <source id="video-source" :src="node.typeData.mediaURL" type="video/mp4" />
-  </video>
 </template>
 
 <script>
@@ -28,18 +43,10 @@ export default {
       required: true,
     },
   },
-  computed: {
-    showEndScreen() {
-      const progress = this.node.typeData.progress[0].value
-      if (progress >= 1) {
-        return true
-      }
-      if (this.$refs.video) {
-        const viewedAmount = progress * this.$refs.video.duration
-        return this.$refs.video.duration <= viewedAmount
-      }
-      return false
-    },
+  data() {
+    return {
+      showEndScreen: this.getInitialEndScreenState(),
+    }
   },
   mounted() {
     /* setTimeout(() => {
@@ -59,6 +66,17 @@ export default {
     }
   },
   methods: {
+    getInitialEndScreenState() {
+      const progress = this.node.typeData.progress[0].value
+      if (progress >= 1) {
+        return true
+      }
+      if (this.$refs.video) {
+        const viewedAmount = progress * this.$refs.video.duration
+        return this.$refs.video.duration <= viewedAmount
+      }
+      return false
+    },
     handlePlay() {
       const { id, mediaType } = this.node
       const video = this.$refs.video
@@ -82,7 +100,7 @@ export default {
 
       const progress = this.node.typeData.progress[0].value
       const viewedAmount = progress * video.duration
-      video.currentTime = progress >= 1 ? video.duration : viewedAmount
+      video.currentTime = viewedAmount
     },
     updateVideoProgress() {
       const video = this.$refs.video
@@ -98,6 +116,10 @@ export default {
         this.$emit("update-skippable", true)
       }
 
+      if (amountViewed >= 1) {
+        this.showEndScreen = true
+      }
+
       thisTapestryTool.updateChildren(this.node.id, video)
       thisTapestryTool.updateProgressBars()
     },
@@ -106,8 +128,62 @@ export default {
 </script>
 
 <style scoped>
+.end-screen {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.9);
+  color: black;
+  opacity: 1;
+  transition: opacity 0.4s ease-out;
+  z-index: 10;
+}
+
+.end-screen--hide {
+  opacity: 0;
+}
+
+.end-screen-button {
+  background: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  color: inherit;
+  margin-right: 3em;
+}
+
+.end-screen-button:last-child {
+  margin-right: 0;
+}
+
+.end-screen-button:hover {
+  color: #11a6d8;
+}
+
+.end-screen-button-text {
+  margin: 0;
+  padding: 0;
+  font-weight: 600;
+  margin-top: 1em;
+}
+
 .video {
+  position: absolute;
+  left: 0;
+  top: 0;
   width: 100%;
   height: auto;
+}
+
+.container {
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 </style>
