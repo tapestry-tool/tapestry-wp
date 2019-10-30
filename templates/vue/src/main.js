667f292205a8feadda47cfdcf2b8ac5340230b0a
Narendras <n.syahrasyad.n@gmail.com>
2019-10-30 10:45:54 -0700

    Installed vuex

--- templates/vue/src/main.js
+++ templates/vue/src/main.js
@@ -1,10 +1,12 @@
-import Vue from "vue"
-import App from "./App.vue"
 import BootstrapVue from "bootstrap-vue"
+import Vue from "vue"
+import Vuex from "vuex"
+import App from "./App.vue"
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
 Vue.use(BootstrapVue)
+Vue.use(Vuex)
 
 new Vue({
   el: "#tapestry-container",
