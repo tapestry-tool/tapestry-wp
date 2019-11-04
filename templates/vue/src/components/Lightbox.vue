43c1cdd74860e3bcdcb4c8786d4ed099447b6516
Narendra Syahrasyad <31267630+narendrasss@users.noreply.github.com>
2019-11-04 10:56:09 -0800

    Merge master into tyde (#186)
    * Updated base branch to master
    
    * Updated base branch to master
    
    * Merge master into this branch
    
    * Improvements- Improve auto-layout behavior (nodes were appearing off the page)- Refresh page on save to apply updated settings- Remove or set fx/fy for nodes when tapestry is loaded depending on if auto-layout is enabled or not
    
    * Fixed skippable not being set on refresh
    
    * Removed console logs and changed skip name
    
    * Fixed spacing changes
    
    * Fixed editor spacing problems
    
    * linted master
    
    * Changed some styling rules
    
    * Added linter
    
    * Put constraints on node placement and size so it appears better to users and authors (#137)
    
    - Limit the maximum size of the nodes
    - Horizontally center nodes when needed
    - Place root node much further left and lower so author can put nodes above it or to its right
    - Do not transpose the nodes if it's the author editing the tapestry as it's annoying
    - Misc. fix: make sure controls show up always at the top
    
    * Bug fix: Showing "Video" instead of "H5P" type when editing node (#159)
    
    * Fixed H5P showing incorrect type due to not taking into account mediaFormat
    
    * Post-release improvements
    - Change minimum depth to 2 instead of 1 to avoid weird issues
    - Set depth to 4 instead of 2 by default
    - Change the text node icon to a play icon wiht T on top of it
    - Refresh page when link is deleted to prevent authoring issues
    
    * Added behaviour tab
    
    * Improve performance related to depth and getChildren function (#162)
    
    * Rewrote getChildren() function to be simpler and use caching
    * Cache children of nodes at depth "n" for faster performance
    * Rewrote addDepthToNodes() function to be simpler
    * Only call addDepthToNodes() function when it's absolutely necessary (on load/reload, or node change)
    
    * Add "authenticated" permission row (#165)
    
    * Added authenticated permission row
    * Updated backend to support authenticated row
    * Completed intended checkbox behaviour
    
    * Fixed and improved permissions (#166)
    
    * Hide settings if user cannot edit post
    * Updated checkboxes to be more intuitive
    * Fix permission checkbox inheritance
    * Hide add root node button if not authorized
    * Allow node edit/add for author of node
    * Allow link add/delete for author of link
    * Add support for author in custom post types
    * Allow saving of node coordinates and free movement for author of the node
    * Reset the tapestry/node author after someone edits the tapestry/node
    
    * Hide video duration when non-video / no duration (#170)
    
    * Ensure videos are skippable by default (#168)
    
    * Refactored lightbox to Vue (#156)
    
    * Removed lightbox code / functionality from tapestry.js
    * Created component for lightbox in view
    * Created components for different content types to open in the lightbox 
    * Created functionality for content to be opened by retrieving data from the API for the individual node being opened (by passing the ID) rather than passing data to the lightbox 
    * Ensure lightbox-related functionality (e.g. saving progress, analytics, dynamic icons and view mode) works with the new Vue lightbox
    
    Fixes #153

---
+++ templates/vue/src/components/Lightbox.vue
@@ -0,0 +1,278 @@
+<template>
+  <div id="lightbox">
+    <div v-if="skippable" id="spotlight-overlay" @click="$emit('close')"></div>
+    <transition name="lightbox">
+      <div
+        v-if="isLoaded"
+        id="spotlight-content"
+        :class="['content', { 'content-text': node.mediaType === 'text' }]"
+        :style="lightboxContentStyles"
+      >
+        <button v-if="skippable" id="lightbox-close-wrapper" @click="$emit('close')">
+          <div class="lightbox-close">
+            <i class="fa fa-times"></i>
+          </div>
+        </button>
+        <div
+          :class="[
+            'media-wrapper',
+            { 'media-wrapper-embed': node.mediaFormat === 'embed' },
+          ]"
+        >
+          <text-media v-if="node.mediaType === 'text'" :node="node" />
+          <video-media
+            v-if="node.mediaFormat === 'mp4'"
+            :node="node"
+            @load="updateDimensions"
+            @update-skippable="updateSkippable"
+            @timeupdate="updateProgress"
+          />
+          <external-media
+            v-if="node.mediaFormat === 'embed'"
+            :node="node"
+            :width="dimensions.width"
+            :height="dimensions.height"
+          />
+          <h5p-media
+            v-if="node.mediaFormat === 'h5p'"
+            :node="node"
+            :width="dimensions.width"
+            :height="dimensions.height"
+            :settings="h5pSettings"
+            @update-settings="updateH5pSettings"
+            @timeupdate="updateProgress"
+          />
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+import TextMedia from "./lightbox/TextMedia"
+import VideoMedia from "./lightbox/VideoMedia"
+import ExternalMedia from "./lightbox/ExternalMedia"
+import H5PMedia from "./lightbox/H5PMedia"
+import Helpers from "../utils/Helpers"
+
+const SAVE_INTERVAL = 5
+
+export default {
+  name: "lightbox",
+  components: {
+    VideoMedia,
+    TextMedia,
+    ExternalMedia,
+    "h5p-media": H5PMedia,
+  },
+  props: {
+    nodeId: {
+      type: String,
+      required: true,
+    },
+    tapestryApiClient: {
+      type: Object,
+      required: true,
+    },
+    h5pSettings: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      node: {},
+      isLoaded: false,
+      dimensions: {
+        top: 100,
+        left: 50,
+      },
+      skippable: false,
+      timeSinceLastSaved: new Date(),
+    }
+  },
+  computed: {
+    lightboxContentStyles() {
+      return {
+        top: this.dimensions.top + "px",
+        left: this.dimensions.left + "px",
+        width: this.dimensions.width + "px",
+        height: this.dimensions.height + "px",
+      }
+    },
+    lightboxDimensions() {
+      const NORMAL_RADIUS = 140 // TODO: Refactor this to "constants" folder
+      if (!this.node) {
+        return {}
+      }
+
+      const { mediaWidth: width, mediaHeight: height } = this.node.typeData
+      const browserWidth = Helpers.getBrowserWidth()
+      const browserHeight = Helpers.getBrowserHeight()
+
+      let resizeRatio = 1
+      let videoWidth = width
+      let videoHeight = height
+
+      if (width > Helpers.getBrowserWidth()) {
+        resizeRatio *= browserWidth / width
+        videoWidth *= resizeRatio
+        videoHeight *= resizeRatio
+      }
+
+      if (videoHeight > browserHeight * resizeRatio) {
+        resizeRatio *= browserHeight / videoHeight
+        videoWidth *= resizeRatio
+        videoHeight *= resizeRatio
+      }
+
+      const nodeSpace = NORMAL_RADIUS * 2 * 1.3
+      const adjustedVideoHeight = Math.min(videoHeight, browserHeight - nodeSpace)
+      const adjustedVideoWidth = Math.min(videoWidth, browserWidth - nodeSpace)
+
+      const heightAdjustmentRatio = adjustedVideoHeight / videoHeight
+      const widthAdjustmentRatio = adjustedVideoWidth / videoWidth
+      let adjustmentRatio = widthAdjustmentRatio
+      let adjustedOn = "width"
+
+      if (Helpers.getAspectRatio() < 1) {
+        adjustedOn = "height"
+        adjustmentRatio = heightAdjustmentRatio
+      }
+
+      adjustmentRatio *= 0.95
+      return {
+        adjustedOn,
+        width: videoWidth * adjustmentRatio,
+        height: videoHeight * adjustmentRatio,
+      }
+    },
+  },
+  async mounted() {
+    const node = await this.tapestryApiClient.getNode(this.nodeId)
+    const meta = await this.tapestryApiClient.getNodeProgress(this.nodeId)
+    node.typeData.progress[0].value = meta.progress
+    node.typeData.progress[1].value = 1.0 - meta.progress
+
+    this.node = node
+    this.skippable = meta.skippable
+    this.isLoaded = true
+    this.dimensions = {
+      ...this.dimensions,
+      left: (Helpers.getBrowserWidth() - this.lightboxDimensions.width) / 2,
+      width: this.lightboxDimensions.width,
+      height: this.lightboxDimensions.height,
+    }
+    thisTapestryTool.changeToViewMode(this.lightboxDimensions)
+  },
+  async beforeDestroy() {
+    await this.tapestryApiClient.updateUserProgress(
+      this.nodeId,
+      this.node && this.node.typeData.progress[0].value
+    )
+    thisTapestryTool.exitViewMode()
+  },
+  methods: {
+    async updateSkippable() {
+      await this.tapestryApiClient.updateSkippable(this.nodeId)
+      this.skippable = true
+    },
+    async updateProgress(type, amountViewed) {
+      const now = new Date()
+      const secondsDiff = Math.abs(
+        (now.getTime() - this.timeSinceLastSaved.getTime()) / 1000
+      )
+      this.$emit("progress", this.nodeId, amountViewed)
+
+      if (secondsDiff > SAVE_INTERVAL) {
+        await this.tapestryApiClient.updateUserProgress(this.nodeId, amountViewed)
+
+        if (type === "h5p") {
+          await this.tapestryApiClient.updateH5pSettings(this.h5pSettings)
+        }
+
+        this.timeSinceLastSaved = now
+      }
+    },
+    updateH5pSettings(newSettings) {
+      this.h5pSettings = newSettings
+    },
+    updateDimensions({ width, height }) {
+      this.dimensions = {
+        ...this.dimensions,
+        width,
+        height,
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+#lightbox {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  opacity: 1;
+  transform: translateY(0);
+}
+
+#spotlight-overlay {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+}
+
+.content {
+  position: absolute;
+  top: 5vh;
+  left: 5vw;
+  height: 90vh;
+  width: 90vw;
+  z-index: 100;
+  background-position: 0 0;
+  background-size: cover;
+  background-color: black;
+  box-shadow: 0 0 100px -40px #000;
+  border-radius: 15px;
+}
+
+.media-wrapper {
+  background: #000;
+  outline: none;
+  border-radius: 15px;
+  overflow: hidden;
+}
+
+.media-wrapper-embed {
+  background: white;
+}
+
+.content-text {
+  outline: none;
+  background: #eee;
+  color: #333;
+  padding: 1em;
+}
+
+.content-text .media-wrapper {
+  height: 100%;
+  overflow: scroll;
+  background: transparent;
+}
+</style>
+
+<style>
+.lightbox-enter-active,
+.lightbox-leave-active {
+  transition: all 1s;
+}
+
+.lightbox-enter,
+.lightbox-leave-to {
+  opacity: 0;
+  transform: translateY(32px);
+}
+</style>
