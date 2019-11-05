e8685be85ed93f473e485f1a5eac5f102e713d11
Narendras <n.syahrasyad.n@gmail.com>
2019-11-05 13:32:31 -0800

    Added module boilerplate

---
+++ templates/vue/src/components/tyde/TydeModule.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :id="`module-${nodeId}`">
+    <div :stage="activeStage" @next="next"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tyde-module",
+  props: {
+    nodeId: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      activeStageIndex: 0,
+    }
+  },
+  computed: {
+    stages() {
+      return []
+    },
+    activeStage() {
+      return this.stages[this.activeStageIndex]
+    },
+  },
+  methods: {
+    next() {
+      this.activeStageIndex++
+    },
+  },
+}
+</script>
+
+<style scoped></style>
