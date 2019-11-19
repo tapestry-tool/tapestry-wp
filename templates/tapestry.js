2df678f768ad5d77f3f979d9893abbc490278bfd
Nanda <n.syahrasyad.n@gmail.com>
2019-11-19 13:17:09 -0800

    Remove const from tapestry js

--- templates/tapestry.js
+++ templates/tapestry.js
@@ -200,13 +200,13 @@ function tapestryTool(config){
     this.canCurrentUserEdit = () => Boolean(config.wpCanEditTapestry.length)
 
     this.init = function(isReload = false) {
-        const reorderPermissions = permissions => {
-            const withoutDuplicates = new Set(["public", "authenticated", ...Object.keys(permissions)])
+        var reorderPermissions = permissions => {
+            var withoutDuplicates = new Set(["public", "authenticated", ...Object.keys(permissions)])
             return [...withoutDuplicates];
         }
 
         this.dataset.nodes = this.dataset.nodes.map(node => {
-            const updatedNode = fillEmptyFields(node, { skippable: true, behaviour: "embed", completed: false, quiz: [] })
+            var updatedNode = fillEmptyFields(node, { skippable: true, behaviour: "embed", completed: false, quiz: [] })
             updatedNode.permissions = fillEmptyFields(
                 updatedNode.permissions, 
                 { authenticated: ["read"] }
@@ -1880,7 +1880,7 @@ function tapestryTool(config){
             var amountViewed = progressObj[id].progress;
             var amountUnviewed = 1.00 - amountViewed;
             var unlocked = progressObj[id].unlocked;
-            const quizCompletionInfo = progressObj[id].quiz;
+            var quizCompletionInfo = progressObj[id].quiz;
         
             var index = findNodeIndex(id);
             
@@ -1890,11 +1890,13 @@ function tapestryTool(config){
                 tapestry.dataset.nodes[index].typeData.progress[1].value = amountUnviewed;
                 tapestry.dataset.nodes[index].unlocked = unlocked ? true : false;
 
-                const questions = tapestry.dataset.nodes[index].quiz;
+                var questions = tapestry.dataset.nodes[index].quiz;
                 if (quizCompletionInfo) {
                     Object.entries(quizCompletionInfo).forEach(([questionId, isCompleted]) => {
-                        const question = questions.find(question => question.id === questionId);
-                        question.completed = isCompleted;
+                        var question = questions.find(question => question.id === questionId);
+                        if (question) {
+                            question.completed = isCompleted;
+                        }
                     })
                 }
             }
