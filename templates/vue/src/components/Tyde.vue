251bf6e5577fc8df168a8f8e12e6e622b05f646c
wynnset <wynnset@niavarani.com>
2019-11-22 15:36:54 -0800

    Improvements - Move all backpack-related code into the new TydeBackpack component - Improve positioning of backpack and some code improvementss

--- templates/vue/src/components/Tyde.vue
+++ templates/vue/src/components/Tyde.vue
@@ -2,38 +2,24 @@
   <div id="tyde">
     <tapestry />
     <tyde-module v-if="showModule" :node-id="moduleId" @done="showModule = false" />
-    <input type="image" class="backpack" :src="backpack" @click="toggleMenu"/>
-    <tyde-menu
-      v-if="isMenuOpen"
-      :logs="logs"
-      @audio-change="toggleAudio"
-      @continue="continueTapestry"
-      @return-to-map="returnToMap"
-    />
+    <tyde-backpack />
   </div>
 </template>
 
 <script>
 import Tapestry from "./Tapestry"
 import TydeModule from "./tyde/TydeModule"
-import TydeMenu from "./tyde/TydeMenu"
-import { mapGetters, mapMutations } from "vuex"
-import Backpack from "@/assets/backpack.svg"
-
-const TYDE_BACKGROUND_AUDIO_SRC =
-  "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3" // test file
+import TydeBackpack from "./tyde/TydeBackpack"
 
 export default {
   name: "tyde",
   components: {
     Tapestry,
-    TydeMenu,
     TydeModule,
+    TydeBackpack,
   },
   data() {
     return {
-      backgroundAudio: new Audio(TYDE_BACKGROUND_AUDIO_SRC),
-      isMenuOpen: false,
       showModule: false,
       moduleId: null,
     }
@@ -43,79 +29,9 @@ export default {
       this.showModule = !this.showModule
       this.moduleId = evt.detail
     })
-    window.addEventListener("keydown", evt => {
-      if (evt.code === "Escape") {
-        const { el } = this.lightbox
-        if (el) {
-          if (this.isMenuOpen && el.currentTime < el.duration) {
-            el.play()
-          } else {
-            el.pause()
-          }
-        }
-        this.toggleMenu()
-      }
-    })
-    this.backgroundAudio.loop = true
-  },
-  beforeDestroy() {
-    window.removeEventListener("keydown")
-  },
-  computed: {
-    ...mapGetters(["lightbox", "nodes"]),
-    backpack() {
-      return `${wpData.vue_uri}/${Backpack.split("dist")[1]}`
-    },
-    logs() {
-      return this.nodes.filter(node => node.completed).map(node => {
-        return {
-          type: "content",
-          title: node.title,
-          imageURL: node.imageURL,
-          description: node.description,
-          isFavourite: node.isFavourite,
-        }
-      })
-    }
-  },
-  methods: {
-    ...mapMutations(["closeLightbox"]),
-    continueTapestry() {
-      const { el } = this.lightbox
-      this.toggleMenu()
-      if (el) {
-        if (el.currentTime < el.duration) {
-          el.play()
-        }
-      }
-    },
-    returnToMap() {
-      this.closeLightbox()
-      this.toggleMenu()
-    },
-    toggleAudio() {
-      if (this.backgroundAudio.paused) {
-        this.backgroundAudio.play()
-      } else {
-        this.backgroundAudio.pause()
-      }
-    },
-    toggleMenu() {
-      this.isMenuOpen = !this.isMenuOpen
-    },
   },
 }
 </script>
-
-<style lang="scss" scoped>
-.backpack {
-  position: absolute;
-  top: 0px;
-  right: 0px;
-  margin: 50px 8%;
-  max-width: 140px;
-}
-</style>
 
 <style>
 :root {
---
+++ templates/vue/src/components/tyde/TydeBackpack.vue
@@ -0,0 +1,108 @@
+<template>
+    <div>
+        <input type="image" id="tyde-backpack-icon" :src="backpackUrl" @click="toggleMenu"/>
+        <tyde-menu
+        v-if="isMenuOpen"
+        :logs="logs"
+        @audio-change="toggleAudio"
+        @continue="continueTapestry"
+        @return-to-map="returnToMap"
+        />
+    </div>
+</template>
+
+<script>
+import TydeMenu from "./TydeMenu"
+import BackpackIcon from "@/assets/backpack.svg"
+import { mapGetters, mapMutations } from "vuex"
+
+const TYDE_BACKGROUND_AUDIO_SRC =
+  "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3" // test file
+
+export default {
+  name: "tyde-backpack",
+  components: {
+    TydeMenu,
+  },
+  data() {
+    return {
+      backgroundAudio: new Audio(TYDE_BACKGROUND_AUDIO_SRC),
+      isMenuOpen: false,
+      escMenuOpenListener: null,
+    }
+  },
+  mounted() {
+    this.escMenuOpenListener = window.addEventListener("keydown", evt => {
+      if (evt.code === "Escape") {
+        const { el } = this.lightbox
+        if (el) {
+          if (this.isMenuOpen && el.currentTime < el.duration) {
+            el.play()
+          } else {
+            el.pause()
+          }
+        }
+        this.toggleMenu()
+      }
+    })
+    this.backgroundAudio.loop = true
+  },
+  computed: {
+    ...mapGetters(["lightbox", "nodes"]),
+    backpackUrl() {
+      return `${wpData.vue_uri}/${BackpackIcon.split("dist")[1]}`
+    },
+    logs() {
+      return this.nodes.filter(node => node.completed).map(node => {
+        return {
+          type: "content",
+          title: node.title,
+          imageURL: node.imageURL,
+          description: node.description,
+          isFavourite: node.isFavourite,
+        }
+      })
+    }
+  },
+  methods: {
+    ...mapMutations(["closeLightbox"]),
+    continueTapestry() {
+      const { el } = this.lightbox
+      this.toggleMenu()
+      if (el) {
+        if (el.currentTime < el.duration) {
+          el.play()
+        }
+      }
+    },
+    returnToMap() {
+      this.closeLightbox()
+      this.toggleMenu()
+    },
+    toggleAudio() {
+      if (this.backgroundAudio.paused) {
+        this.backgroundAudio.play()
+      } else {
+        this.backgroundAudio.pause()
+      }
+    },
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen
+    },
+  },
+  beforeDestroy() {
+    window.removeEventListener(this.escMenuOpenListener)
+  },
+}
+</script>
+
+
+<style lang="scss" scoped>
+#tyde-backpack-icon {
+  position: absolute;
+  top: 0px;
+  right: 5%;
+  max-width: 140px;
+  outline: none;
+}
+</style>
