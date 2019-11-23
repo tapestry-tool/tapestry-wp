0dbd880082eb73aa7344ba239dfe24613c535d65
Andrew Bui <andrewbui130@hotmail.com>
2019-11-23 12:13:06 -0800

    Add audio component

---
+++ templates/vue/src/components/AudioPlayer.vue
@@ -0,0 +1,23 @@
+<template>
+  <audio controls="controls">
+    Your browser does not support the
+    <code>audio</code> element.
+    <source v-bind:src="audioSrc" />
+  </audio>
+</template>
+
+<script>
+
+export default {
+  name: "audio-player",
+  props: {
+    audioSrc: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
