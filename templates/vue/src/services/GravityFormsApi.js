e68f930991210ac916b1c6122149a38ac32f37ce
Nanda <n.syahrasyad.n@gmail.com>
2019-11-25 12:50:36 -0800

    Update code styling

--- templates/vue/src/services/GravityFormsApi.js
+++ templates/vue/src/services/GravityFormsApi.js
@@ -4,13 +4,21 @@ const API_URL = wpData.gf_rest_url
 
 const makeUrl = path => `${API_URL}${path}?_gf_json_nonce=${wpData.gf_nonce}`
 
+const request = (method, endpoint) => {
+  return axios
+    .request({ url: makeUrl(endpoint), method })
+    .then(response => response.data)
+    .then(({ status, response }) =>
+      status >= 400 ? Promise.reject(response) : response
+    )
+}
+
 export default {
   /**
    * Get entries for a given `formId`
    * @param {string | number} formId
    */
   getEntries: function(formId) {
-    const url = makeUrl(`/forms/${formId}`)
-    return axios.get(url).then(response => response.data)
+    return request("GET", `/forms/${formId}`)
   },
 }
