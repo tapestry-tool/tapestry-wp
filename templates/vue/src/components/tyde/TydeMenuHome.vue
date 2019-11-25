d9e658276c167d0c1cd0e29ff13652963c7e9b7c
Andrew Bui <andrewbui130@hotmail.com>
2019-11-24 19:40:16 -0800

    Check if the answer is recorded on the server in the log

--- templates/vue/src/components/tyde/TydeMenuHome.vue
+++ templates/vue/src/components/tyde/TydeMenuHome.vue
@@ -21,6 +21,10 @@
 <script>
 import TydeLog from "./TydeLog"
 import TydeTab from "./TydeTab"
+import TapestryApi from "@/services/TapestryAPI"
+import Helpers from "@/utils/Helpers"
+
+const TapestryApiClient = new TapestryApi(wpPostId)
 
 export default {
   name: "tyde-menu-home",
@@ -38,6 +42,7 @@ export default {
   data() {
     return {
       activeTab: "all",
+      logsWithAnswers: [],
       tabs: ["all", "activities", "content" /*, "favourites"*/],
     }
   },
@@ -45,22 +50,52 @@ export default {
     visibleLogs() {
       const filter = this.activeTab
       if (filter === "activities") {
-        return this.logs.filter(item => item.type === "activity")
+        return this.logsWithAnswers.filter(item => item.type === "activity")
       }
       if (filter === "content") {
-        return this.logs.filter(item => item.type === "content")
+        return this.logsWithAnswers.filter(item => item.type === "content")
       }
       if (filter === "favourites") {
-        return this.logs.filter(item => item.isFavourite)
+        return this.logsWithAnswers.filter(item => item.isFavourite)
       }
-      return this.logs
+      return this.logsWithAnswers
     },
   },
   methods: {
     setActiveTab(tab) {
       this.activeTab = tab
     },
+    async getAudioSrc(nodeId, audioId) {
+      try {
+        const audio = await TapestryApiClient.getH5PAudioFromServer(nodeId, audioId)
+        const blob = await Helpers.base64ToBlob(audio, 'audio/wav')
+        return URL.createObjectURL(blob)
+      } catch (e) {
+        console.error(e)
+        return ""
+      }
+    }
   },
+  watch: {
+    /**
+     * Further filter the logs to check if there are valid answers recorded
+     * on the server or not. If not, don't show those logs.
+     */
+    logs: {
+      immediate: true,
+      handler(newLogs) {
+        newLogs.forEach(async log => {
+          if (log.audioId) {
+            const audioSrc = await this.getAudioSrc(log.nodeId, log.audioId)
+            if (audioSrc)
+              this.logsWithAnswers.push({ ...log, audioSrc })
+          } else {
+            this.logsWithAnswers.push({ ...log })
+          }
+        })
+      }
+    }
+  }
 }
 </script>
 
