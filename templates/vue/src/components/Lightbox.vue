3cccc465374fd85b67c9b8d24bb7c8fb3bd0eb93
Adel <adel-s@outlook.com>
2020-02-04 12:04:22 -0800

    Made scrollbar larger for webkit devices. Fixed issue with scrollbar-width

--- templates/vue/src/components/Lightbox.vue
+++ templates/vue/src/components/Lightbox.vue
@@ -202,12 +202,12 @@ export default {
     box-shadow: 0 0 100px -40px #000;
     border-radius: 15px;
     scrollbar-color: rgba(0,0,0,.5) rgba(255,255,255,.5) ;
-    scrollbar-width: 7px;
+    scrollbar-width: auto;
     -ms-overflow-style:scrollbar;
 
     ::-webkit-scrollbar {
       -webkit-appearance: none;
-      width: 7px;
+      width: 12px;
     }
 
     ::-webkit-scrollbar-thumb {
