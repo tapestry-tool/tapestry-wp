41ac23d1f18fc7ba292a5d39e67f42b72d3f5b94
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-03-27 11:10:28 -0700

    Allow saving of teen id field if user is a copilot

--- tapestry.php
+++ tapestry.php
@@ -101,14 +101,17 @@ function create_copilot_role()
 }
 add_action('init', 'create_copilot_role');
 
+/**
+ * Show the teen input label if the user is a copilot
+ */
 function add_copilot_teen_field($user) 
 { 
     if (in_array("copilot", $user->roles)): ?>
         <table class="form-table">
         <tr>
-            <th><label for="teenId"><?php _e("Teen ID"); ?></label></th>
+            <th><label for="teen_id"><?php _e("Teen ID"); ?></label></th>
             <td>
-                <input type="text" name="teenId" id="teenId" value="<?php echo esc_attr( get_the_author_meta('teenId', $user->ID)); ?>" class="regular-text" /><br />
+                <input type="text" name="teen_id" id="teen_id" value="<?php echo esc_attr( get_the_author_meta('teen_id', $user->ID)); ?>" class="regular-text" /><br />
                 <span class="description"><?php _e("Please enter your teen's user ID."); ?></span>
             </td>
         </tr>
@@ -117,6 +120,20 @@ function add_copilot_teen_field($user)
 }
 add_action('show_user_profile', 'add_copilot_teen_field');
 add_action('edit_user_profile', 'add_copilot_teen_field');
+
+function save_copilot_teen_field($user_id)
+{
+    if (!current_user_can('edit_user', $user_id)) {
+        return false;
+    }
+    $user = get_user_by('id', $user_id);
+    if (!in_array('copilot', $user->roles)) {
+        return false;
+    }
+    update_user_meta($user_id, 'teen_id', $_POST['teen_id']);
+}
+add_action('personal_options_update', 'save_copilot_teen_field');
+add_action('edit_user_profile_update', 'save_copilot_teen_field');
 
 /**
  * Show posts of Tapestry type on the home page
