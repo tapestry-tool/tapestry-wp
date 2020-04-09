682aaea72fc25cf6717939127f8caa171b9c1eea
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-04-09 13:50:59 -0700

    Add duration text

--- templates/vue/src/components/AudioRecorder.vue
+++ templates/vue/src/components/AudioRecorder.vue
@@ -3,12 +3,19 @@
     Oops, your browser doesn't support audio recording.
   </div>
   <div v-else>
-    <button @click="startRecording">Start</button>
-    <button @click="toggleRecording">
-      {{ state === states.PAUSED ? "Play" : "Pause" }}
-    </button>
-    <button @click="stopRecording">Stop</button>
-    <audio v-if="state === states.DONE" controls :src="audio"></audio>
+    <div class="controls">
+      <button @click="startRecording">Start</button>
+      <button @click="toggleRecording">
+        {{ state === states.PAUSED ? "Play" : "Pause" }}
+      </button>
+      <button @click="stopRecording">Stop</button>
+    </div>
+    <div class="content">
+      <div>
+        <code>{{ durationText }}</code>
+      </div>
+      <audio v-if="state === states.DONE" controls :src="audio"></audio>
+    </div>
   </div>
 </template>
 
@@ -33,11 +40,28 @@ export default {
     return {
       audio: null,
       data: [],
+      duration: 0,
+      durationInterval: null,
       recorder: null,
       state: null,
     }
   },
   computed: {
+    durationText() {
+      let hours = Math.floor(this.duration / 3600)
+      let minutes = Math.floor((this.duration - hours * 3600) / 60)
+      let sec = this.duration - hours * 3600 - minutes * 60
+      if (sec < 10) {
+        sec = `0${sec}`
+      }
+      if (hours > 0 && minutes < 10) {
+        minutes = `0${minutes}`
+      }
+      if (hours === 0) {
+        return `${minutes}:${sec}`
+      }
+      return `${hours}:${minutes}:${sec}`
+    },
     states() {
       return {
         DONE: "done",
@@ -57,8 +81,14 @@ export default {
     }
   },
   methods: {
+    startDurationCount() {
+      this.durationInterval = setInterval(() => {
+        this.duration++
+      }, 1000)
+    },
     startRecording() {
       this.data = []
+      this.duration = 0
       navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
         const recorder = new MediaRecorder(stream)
 
@@ -79,18 +109,25 @@ export default {
 
         this.recorder = recorder
         this.recorder.start()
+        this.startDurationCount()
         this.state = this.states.RECORDING
       })
     },
+    stopDurationCount() {
+      clearInterval(this.durationInterval)
+    },
     stopRecording() {
       this.recorder.stop()
+      this.stopDurationCount()
     },
     toggleRecording() {
       if (this.state === this.states.RECORDING) {
         this.recorder.pause()
+        this.stopDurationCount()
         this.state = this.states.PAUSED
       } else if (this.state === this.states.PAUSED) {
         this.recorder.resume()
+        this.startDurationCount()
         this.state = this.states.RECORDING
       }
     },
