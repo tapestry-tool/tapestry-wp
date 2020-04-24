dfb66914bb8155f360e8afcf1d3e48982546b182
Narendra Syahrasyad <31267630+narendrasss@users.noreply.github.com>
2020-04-23 17:11:55 -0700

    Bug Fix: Not able to delete combobox values (#448)

--- templates/vue/src/components/Combobox.vue
+++ templates/vue/src/components/Combobox.vue
@@ -47,7 +47,8 @@ export default {
     },
     value: {
       type: [Object, String],
-      required: true,
+      required: false,
+      default: null,
     },
     emptyMessage: {
       type: String,
@@ -104,9 +105,10 @@ export default {
   methods: {
     handleBlur() {
       this.isOpen = false
-      // if user leaves focus and hasn't selected anything,
-      // revert to whatever the previous selection was.
-      if (this.inputValue !== this.text && !this.selected) {
+      // if user leaves focus and input is empty, clear the value
+      if (this.inputValue.length === 0) {
+        this.$emit("input", null)
+      } else if (this.inputValue !== this.text && !this.selected) {
         this.inputValue = this.text
       }
     },
