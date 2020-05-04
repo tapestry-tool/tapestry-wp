fdd9427323797f0615afc5713bda56929f2372de
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-05-04 16:36:37 -0700

    Add stub filter component

---
+++ templates/vue/src/components/TapestryFilter.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>I'm a filter</h1>
+</template>
+
+<script>
+export default {
+  name: "tapestry-filter",
+  watch: {
+    $route(to, from) {
+      console.log(to, from)
+    },
+  },
+}
+</script>
