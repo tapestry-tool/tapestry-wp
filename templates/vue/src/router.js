6f3eb713ea88bb71b432ccf255eb815f50421706
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-05-04 16:36:37 -0700

    Add stub filter component

--- templates/vue/src/router.js
+++ templates/vue/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue"
 import VueRouter from "vue-router"
 import Lightbox from "./components/Lightbox"
+import TapestryFilter from "./components/TapestryFilter"
 
 Vue.use(VueRouter)
 
@@ -8,6 +9,16 @@ const routes = [
   {
     path: "/nodes/:nodeId",
     component: Lightbox,
+    props: true,
+  },
+  {
+    path: "/filter",
+    component: TapestryFilter,
+    props: true,
+  },
+  {
+    path: "/",
+    component: TapestryFilter,
     props: true,
   },
 ]
