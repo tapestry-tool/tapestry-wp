fbf1d82ff1f59f18e6f00a88f9191994e93664b1
wynnset <wynnset@niavarani.com>
2020-05-07 18:34:39 -0700

    Unchecking checkboxes in authoring tool should clear corresponding content

--- templates/vue/src/components/node-modal/ConditionsForm.vue
+++ templates/vue/src/components/node-modal/ConditionsForm.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-form-group class="mb-3">
-      <b-form-checkbox v-model="lock" @change="disableConditions">
+      <b-form-checkbox v-model="lock" @change="$emit('changed', !lock)">
         Prevent access until specified conditions are met
       </b-form-checkbox>
     </b-form-group>
@@ -96,11 +96,6 @@ export default {
     },
     removeCondition(idx) {
       this.conditions.splice(idx, 1)
-    },
-    disableConditions() {
-      if (this.lock) {
-        this.conditions = []
-      }
     },
   },
 }
