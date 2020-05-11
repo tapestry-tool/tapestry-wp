9779da672eea4e6128e910e7062630100fd137d2
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-05-11 14:32:27 -0700

    Remove quiz and typeData attributes for locked nodes

--- classes/class.constants.php
+++ classes/class.constants.php
@@ -7,3 +7,10 @@ class TydeTypes
     const STAGE = "Stage";
     const QUESTION_SET = "Question set";
 }
+
+class ConditionTypes
+{
+    const NODE_COMPLETED = "node_completed";
+    const DATE_NOT_PASSED = "date_not_passed";
+    const DATE_PASSED = "date_passed";
+}
