36f95cbbf9d140c57533f27421ea4bae1c8534e8
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-05-11 11:11:34 -0700

    Fix audio recorder build error

--- templates/vue/webpack.config.js
+++ templates/vue/webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules(?!\/audio-recorder-polyfill)/
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
