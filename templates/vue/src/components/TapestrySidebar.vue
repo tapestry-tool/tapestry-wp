fccef5841e9b1983e0827fec64e75ac38a9230ff
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-06-01 13:57:15 -0700

    Add transition animations

--- templates/vue/src/components/TapestrySidebar.vue
+++ templates/vue/src/components/TapestrySidebar.vue
@@ -1,6 +1,6 @@
 <template>
-  <aside class="sidebar">
-    {{ node.title }}
+  <aside :class="['sidebar', { closed: isClosed }]" @click="isClosed = !isClosed">
+    <p>{{ node.title }}</p>
   </aside>
 </template>
 
@@ -10,7 +10,7 @@ import { mapGetters, mapState } from "vuex"
 export default {
   data() {
     return {
-      isClosed: false,
+      isClosed: true,
     }
   },
   computed: {
@@ -20,16 +20,39 @@ export default {
       return this.getNode(this.selectedNodeId)
     },
   },
+  watch: {
+    isClosed: {
+      immediate: true,
+      handler(isClosed) {
+        const tapestryContainer = document.getElementById("tapestry")
+        if (isClosed) {
+          tapestryContainer.classList.remove("sidebar-open")
+        } else {
+          tapestryContainer.classList.add("sidebar-open")
+        }
+      },
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .sidebar {
+  background: var(--gray);
+  color: white;
   height: 100vh;
+  min-width: 300px;
+  padding: 3rem 0;
   position: fixed;
   right: 0;
   top: 0;
-  padding: 48px 1em 48px 1em;
-  background: var(--gray);
+  transform: translateX(0);
+  transition: all 0.2s ease-out;
+  width: 30vw;
+
+  &.closed {
+    cursor: pointer;
+    transform: translateX(90%);
+  }
 }
 </style>
