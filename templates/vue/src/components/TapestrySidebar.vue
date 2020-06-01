333dd21186b908211d97f86913955acbe6a7545c
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-06-01 14:30:15 -0700

    Add sidebar preview section

--- templates/vue/src/components/TapestrySidebar.vue
+++ templates/vue/src/components/TapestrySidebar.vue
@@ -1,13 +1,24 @@
 <template>
   <aside :class="['sidebar', { closed: isClosed }]" @click="isClosed = !isClosed">
-    <p>{{ node.title }}</p>
+    <div v-show="isClosed" class="sidebar-preview">
+      <tapestry-icon icon="angle-double-left" />
+      <p class="title">{{ node.title }}</p>
+    </div>
+    <div class="sidebar-content">
+      <h3>{{ node.title }}</h3>
+      <p>{{ node.description }}</p>
+    </div>
   </aside>
 </template>
 
 <script>
 import { mapGetters, mapState } from "vuex"
+import TapestryIcon from "@/components/TapestryIcon"
 
 export default {
+  components: {
+    TapestryIcon,
+  },
   data() {
     return {
       isClosed: true,
@@ -40,6 +51,8 @@ export default {
 .sidebar {
   background: var(--gray);
   color: white;
+  display: grid;
+  grid-template-columns: 10% 80% 10%;
   height: 100vh;
   min-width: 300px;
   padding: 3rem 0;
@@ -54,5 +67,28 @@ export default {
     cursor: pointer;
     transform: translateX(90%);
   }
+}
+
+.sidebar-preview {
+  height: 100vh;
+  padding: 48px 0;
+  padding-left: 1em;
+  position: absolute;
+
+  i {
+    display: block;
+    text-align: left;
+  }
+
+  .title {
+    display: block;
+    margin: 0;
+    transform: translateX(8px) rotate(90deg);
+    transform-origin: left center;
+  }
+}
+
+.sidebar-content {
+  grid-column: 2;
 }
 </style>
