59e5b68b91132a0e61756d4f8998b1db7ff00127
Narendra Syahrasyad <n.syahrasyad.n@gmail.com>
2020-06-04 21:12:57 -0700

    Add skeleton depth slider component

---
+++ templates/vue/src/components/TapestryDepthSlider.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input v-model="currentDepth" type="range" min="1" :max="maxDepth" />
+    <p v-if="currentDepth < maxDepth">
+      Some nodes might be hidden because you're not at max depth.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentDepth: 0,
+    }
+  },
+  computed: {
+    maxDepth() {
+      return 4
+    },
+  },
+  watch: {
+    currentDepth: {
+      immediate: true,
+      handler: function() {
+        // TODO: Update node visibility type here
+      },
+    },
+  },
+}
+</script>
