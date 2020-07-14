ece1f0964aee1a9e25042b9a5fb97a8c57076351
seanRong <seanRong@users.noreply.github.com>
2020-07-14 15:48:54 -0700

    template reversion to not use v-for

--- templates/vue/src/components/node-modal/content-form/RichTextForm.vue
+++ templates/vue/src/components/node-modal/content-form/RichTextForm.vue
@@ -3,23 +3,97 @@
     <editor-menu-bar v-slot="{ commands, isActive }" :editor="editor">
       <div class="menubar">
         <button
-          v-for="icon of icons_first_group"
-          :key="icon.vclass"
           class="menubar__button"
-          :class="icon.vclass"
-          @click="icon.click"
+          :class="{ 'is-active': isActive.bold() }"
+          @click="commands.bold"
         >
-          <icon v-if="icon.name" :name="icon.name" />
-          {{ icon.text }}
+          <icon name="bold" />
         </button>
-
         <button
-          v-for="icon of icons_second_group"
-          :key="icon.name"
           class="menubar__button"
-          @click="icon.click"
+          :class="{ 'is-active': isActive.italic() }"
+          @click="commands.italic"
         >
-          <icon :name="icon.name" />
+          <icon name="italic" />
+        </button>
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.strike() }"
+          @click="commands.strike"
+        >
+          <icon name="strike" />
+        </button>
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.underline() }"
+          @click="commands.underline"
+        >
+          <icon name="underline" />
+        </button>
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.code() }"
+          @click="commands.code"
+        >
+          <icon name="code" />
+        </button>
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.paragraph() }"
+          @click="commands.paragraph"
+        >
+          <icon name="paragraph" />
+        </button>
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 2 }) }"
+          @click="commands.heading({ level: 2 })"
+        >
+          H1
+        </button>
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 3 }) }"
+          @click="commands.heading({ level: 3 })"
+        >
+          H2
+        </button>
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.heading({ level: 4 }) }"
+          @click="commands.heading({ level: 4 })"
+        >
+          H3
+        </button>
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.bullet_list() }"
+          @click="commands.bullet_list"
+        >
+          <icon name="ul" />
+        </button>
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.ordered_list() }"
+          @click="commands.ordered_list"
+        >
+          <icon name="ol" />
+        </button>
+        <button
+          class="menubar__button"
+          :class="{ 'is-active': isActive.blockquote() }"
+          @click="commands.blockquote"
+        >
+          <icon name="quote" />
+        </button>
+        <button class="menubar__button" @click="commands.horizontal_rule">
+          <icon name="hr" />
+        </button>
+        <button class="menubar__button" @click="commands.undo">
+          <icon name="undo" />
+        </button>
+        <button class="menubar__button" @click="commands.redo">
+          <icon name="redo" />
         </button>
       </div>
     </editor-menu-bar>
@@ -75,94 +149,6 @@ export default {
   data() {
     return {
       editor: null,
-      icons_first_group: [
-        {
-          vclass: "{ 'is-active': isActive.bold() }",
-          click: "commands.bold",
-          name: "bold",
-          text: "",
-        },
-        {
-          vclass: "{ 'is-active': isActive.italic() }",
-          click: "commands.italic",
-          name: "italic",
-          text: "",
-        },
-        {
-          vclass: "{ 'is-active': isActive.strike() }",
-          click: "commands.strike",
-          name: "strike",
-          text: "",
-        },
-        {
-          vclass: "{ 'is-active': isActive.underline() }",
-          click: "commands.underline",
-          name: "underline",
-          text: "",
-        },
-        {
-          vclass: "{ 'is-active': isActive.code() }",
-          click: "commands.code",
-          name: "code",
-          text: "",
-        },
-        {
-          vclass: "{ 'is-active': isActive.paragraph() }",
-          click: "commands.paragraph",
-          name: "paragraph",
-          text: "",
-        },
-        {
-          vclass: "{ 'is-active': isActive.heading({ level: 2 }) }",
-          click: "commands.heading({ level: 2 })",
-          name: false,
-          text: "H1",
-        },
-        {
-          vclass: "{ 'is-active': isActive.heading({ level: 3 }) }",
-          click: "commands.heading({ level: 3 })",
-          name: false,
-          text: "H2",
-        },
-        {
-          vclass: "{ 'is-active': isActive.heading({ level: 4 }) }",
-          click: "commands.heading({ level: 4 })",
-          name: false,
-          text: "H3",
-        },
-        {
-          vclass: "{ 'is-active': isActive.bullet_list() }",
-          click: "commands.bullet_list",
-          name: "ul",
-          text: "",
-        },
-        {
-          vclass: "{ 'is-active': isActive.ordered_list() }",
-          click: "commands.ordered_list",
-          name: "ol",
-          text: "",
-        },
-        {
-          vclass: "{ 'is-active': isActive.blockquote() }",
-          click: "commands.blockquote",
-          name: "quote",
-          text: "",
-        },
-      ],
-      icons_second_group: [
-        {
-          click: "commands.horizontal_rule",
-          name: "hr",
-        },
-        {
-          click: "commands.undo",
-          name: "undo",
-        },
-        {
-          click: "commands.redo",
-          name: "redo",
-        },
-      ],
     }
   },
   watch: {
