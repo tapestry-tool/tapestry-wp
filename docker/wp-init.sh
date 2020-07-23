34a226c4295f3bfa490d6ac94869f51b5bf1a32f
Narendra Syahrasyad <nanda.s@hey.com>
2020-07-23 12:51:21 -0700

    Update to use wp cli

--- docker/wp-init.sh
+++ docker/wp-init.sh
@@ -5,6 +5,9 @@ rm -f /usr/local/bin/docker-entrypoint.temp.sh
 echo 'mkdir -p /var/www/html/wp-content/uploads' >> /usr/local/bin/docker-entrypoint.sh
 echo 'chown www-data:www-data /var/www/html/wp-content/uploads' >> /usr/local/bin/docker-entrypoint.sh
 echo 'chmod 0755 /var/www/html/wp-content/uploads' >> /usr/local/bin/docker-entrypoint.sh
+echo 'curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar' >> /usr/local/bin/docker-entrypoint.sh
+echo 'chmod +x wp-cli.phar' >> /usr/local/bin/docker-entrypoint.sh
+echo 'mv wp-cli.phar /usr/local/bin/wp' >> /usr/local/bin/docker-entrypoint.sh
 echo 'exec "$@"' >> /usr/local/bin/docker-entrypoint.sh
 
 # execute apache
