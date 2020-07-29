1749e865679af41f6b8babd02a3251f3cc2c8954
seanRong <seanRong@users.noreply.github.com>
2020-07-28 17:36:43 -0700

    gets all posts now in api call

--- templates/vue/src/services/WordpressApi.js
+++ templates/vue/src/services/WordpressApi.js
@@ -3,10 +3,19 @@ import axios from "axios"
 const API_URL = `${wpData.rest_url}/wp/v2`
 
 async function getPosts() {
-  return axios.get(`${API_URL}/posts`).then(res => {
-    const { data } = res
-    return data.map(parsePost)
-  })
+  let totalPages
+  let posts = []
+  const res = await axios.get(`${API_URL}/posts`)
+  console.log(res.headers["x-wp-totalpages"])
+  totalPages = res.headers["x-wp-totalpages"]
+  console.log("total pages" + totalPages)
+  for (let i = 1; i <= totalPages; i++) {
+    const res = await axios.get(`${API_URL}/posts?page=${i}`)
+    console.log(res.data)
+    posts = posts.concat(res.data)
+  }
+  console.log(posts)
+  return posts.map(parsePost)
 }
 
 async function getPostById(id) {
