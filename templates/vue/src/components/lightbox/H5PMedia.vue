7d113150d8c399918d738d8d1d22d1875aaa03b8
seanRong <seanRong@users.noreply.github.com>
2020-08-13 18:26:17 -0700

    fix bug causing h5p to display oddly after a refresh

--- templates/vue/src/components/lightbox/H5PMedia.vue
+++ templates/vue/src/components/lightbox/H5PMedia.vue
@@ -69,6 +69,9 @@ export default {
       showPlayScreen: !this.autoplay,
     }
   },
+  beforeDestroy() {
+    thisTapestryTool.init(true)
+  },
   computed: {
     ...mapState(["h5pSettings"]),
   },
