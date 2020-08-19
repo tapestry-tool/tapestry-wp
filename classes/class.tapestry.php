00fb40f83cf6816036751e1ee808757b6e480751
Narendra Syahrasyad <nanda.s@hey.com>
2020-08-19 12:16:48 -0700

    PR Fix: Revert php changes

--- classes/class.tapestry.php
+++ classes/class.tapestry.php
@@ -342,15 +342,17 @@ class Tapestry implements ITapestry
         $node->accessible = $node->unlocked;
         if ($node->accessible) {
             $neighbourIds = $this->_getNeighbours($node);
-            $neighbours = [];
 
-            foreach ($neighbourIds as $nodeId) {
-                foreach ($nodeList as $otherNode) {
-                    if ($otherNode->id === $nodeId) {
-                        array_push($neighbours, $otherNode);
+            $neighbours = array_map(
+                function ($nodeId) use ($nodeList) {
+                    foreach ($nodeList as $otherNode) {
+                        if ($otherNode->id === $nodeId) {
+                            return $otherNode;
+                        }
                     }
-                }
-            }
+                },
+                $neighbourIds
+            );
 
             foreach ($neighbours as $neighbour) {
                 if (!in_array($neighbour, $visited)) {
