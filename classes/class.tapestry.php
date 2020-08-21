426bc9e900784c1eae895ed6b0e628646f766b26
Narendra Syahrasyad <nanda.s@hey.com>
2020-08-21 12:57:17 -0700

    Move cypress and app out of docker

--- classes/class.tapestry.php
+++ classes/class.tapestry.php
@@ -400,7 +400,7 @@ class Tapestry implements ITapestry
 
         return $tapestry;
     }
-    
+
     private function _getDefaultSettings()
     {
         $post = get_post($this->postId);
@@ -415,6 +415,7 @@ class Tapestry implements ITapestry
         $settings->defaultPermissions = TapestryNodePermissions::getDefaultNodePermissions($this->postId);
         $settings->superuserOverridePermissions = true;
         $settings->permalink = get_permalink($this->postId);
+
         return $settings;
     }
 
