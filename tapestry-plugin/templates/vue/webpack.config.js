701b9c4ad75d37530b364590fd183171f4d56487
Narendra Syahrasyad <nanda.s@hey.com>
2020-08-24 13:57:48 -0700

    Add link preview config and fix build errors

--- tapestry-plugin/templates/vue/webpack.config.js
+++ tapestry-plugin/templates/vue/webpack.config.js
@@ -1,92 +1,88 @@
-var path = require('path')
-var webpack = require('webpack')
-require('babel-polyfill');
+var path = require("path")
+var webpack = require("webpack")
+require("babel-polyfill")
 
 module.exports = {
-  entry: ['babel-polyfill', './src/main.js'],
+  entry: ["babel-polyfill", "./src/main.js"],
   output: {
-    path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
-    filename: 'build.js'
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "/dist/",
+    filename: "build.js",
   },
   module: {
     rules: [
       {
         test: /\.s[a|c]ss$/,
-        loader: 'style!css!sass'
+        loader: "style!css!sass",
       },
       {
         test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
+        use: ["vue-style-loader", "css-loader"],
       },
       {
         test: /\.vue$/,
-        loader: 'vue-loader',
+        loader: "vue-loader",
         options: {
-          loaders: {
-          }
+          loaders: {},
           // other vue-loader options go here
-        }
+        },
       },
       {
         test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules\/(?!(audio-recorder-polyfill|tiptap-commands|highlight.js)\/).*/
+        loader: "babel-loader",
+        exclude: /node_modules\/(?!(audio-recorder-polyfill|tiptap|tiptap-commands|highlight.js)\/).*/,
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          name: '[name].[ext]?[hash]'
-        }
-      }
-    ]
+          name: "[name].[ext]?[hash]",
+        },
+      },
+    ],
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': path.resolve(__dirname, 'src'),
+      vue$: "vue/dist/vue.esm.js",
+      "@": path.resolve(__dirname, "src"),
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    extensions: ["*", ".js", ".vue", ".json"],
   },
   devServer: {
     historyApiFallback: true,
     noInfo: true,
     overlay: true,
-    headers: { 'Access-Control-Allow-Origin': '*' },
+    headers: { "Access-Control-Allow-Origin": "*" },
     hot: true,
-    port: 8080
+    port: 8080,
   },
   performance: {
-    hints: false
+    hints: false,
   },
-  devtool: '#eval-source-map'
+  devtool: "#eval-source-map",
 }
 
-if (process.env.NODE_ENV === 'development') {
-  module.exports.output.publicPath = 'http://localhost:8080/dist/'
+if (process.env.NODE_ENV === "development") {
+  module.exports.output.publicPath = "http://localhost:8080/dist/"
 }
 
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+if (process.env.NODE_ENV === "production") {
+  module.exports.devtool = "#source-map"
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
+      "process.env": {
+        NODE_ENV: '"production"',
+      },
     }),
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
-        warnings: false
-      }
+        warnings: false,
+      },
     }),
     new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
+      minimize: true,
+    }),
   ])
 }
