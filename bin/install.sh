705420ceb28e07c7c87069baf95e185600048d4b
Narendra Syahrasyad <nanda.s@hey.com>
2020-08-24 12:53:19 -0700

    Update folder structure with new docker config

---
+++ bin/install.sh
@@ -0,0 +1,13 @@
+alias wp="docker-compose run --rm wpcli"
+
+wp core install --url=localhost:8000 --admin_user=admin --admin_email=foo@bar.com --admin_password=secret --title=tapestry --debug
+
+wp plugin install h5p
+
+wp plugin activate tapestry-wp h5p
+
+wp user create subscriber sub@foo.com --role=subscriber --user_pass=subscriber
+
+wp rewrite structure %postname%
+
+wp theme install twentynineteen --activate
