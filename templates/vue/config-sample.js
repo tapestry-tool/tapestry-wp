199c28ff3da71fcb55a279fc5b914f1b239cb372
rlnsy <rowlindsay@gmail.com>
2020-09-21 22:03:41 -0700

    read link preview API key from .env

--- templates/vue/config-sample.js
+++
@@ -1,1 +0,0 @@
-export const LINK_PREVIEW_API_KEY = ""
--- templates/vue/src/services/LinkPreviewApi.js
+++ templates/vue/src/services/LinkPreviewApi.js
@@ -5,6 +5,10 @@ import Helpers from "../utils/Helpers"
 const API_URL = `${location.protocol}//api.linkpreview.net`
 
 export async function getLinkMetadata(url) {
+  if (!process.env.LINK_PREVIEW_API_KEY) {
+    console.error("No Link preview api key")
+    return []
+  }
   const endpoint = `${API_URL}/?key=${LINK_PREVIEW_API_KEY}&q=${Helpers.normalizeUrl(
     url
   )}`
