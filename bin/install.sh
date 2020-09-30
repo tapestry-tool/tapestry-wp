1fa26939076ce7dc587c93db1bbb041a6c1ae8b3
rlnsy <rowlindsay@gmail.com>
2020-09-30 10:47:00 -0700

    add explanatory message for the case where container access fails

--- bin/install.sh
+++ bin/install.sh
@@ -3,6 +3,13 @@ alias wp="docker-compose run --rm wpcli --path=/var/www/html"
 # Install wordpress
 wp core install --url=localhost:8000 --admin_user=admin --admin_email=foo@bar.com --admin_password=secret --title=tapestry --debug
 
+if [ $? != 0 ]; then
+    echo "Error installing WordPress! Either the CLI is not yet available \
+or the database is not accessible. Try increasing the container delay setting \
+in 'config.sh'. You should run 'make-uninstall' before trying again.\n"
+    exit 1
+fi
+
 # Install H5P
 wp plugin install h5p
 
