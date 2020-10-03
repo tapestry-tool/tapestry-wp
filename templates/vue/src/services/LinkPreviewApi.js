50a300badb33a4913e2e767aaec662c2467203cb
rlnsy <rowlindsay@gmail.com>
2020-10-03 16:02:56 -0700

    remove old import of config file and update usage

--- templates/vue/src/services/LinkPreviewApi.js
+++ templates/vue/src/services/LinkPreviewApi.js
@@ -1,5 +1,4 @@
 import axios from "axios"
-import { LINK_PREVIEW_API_KEY } from "../../config"
 import Helpers from "../utils/Helpers"
 
 const API_URL = `${location.protocol}//api.linkpreview.net`
@@ -9,9 +8,9 @@ export async function getLinkMetadata(url) {
     console.error("No Link preview api key")
     return []
   }
-  const endpoint = `${API_URL}/?key=${LINK_PREVIEW_API_KEY}&q=${Helpers.normalizeUrl(
-    url
-  )}`
+  const endpoint = `${API_URL}/?key=${
+    process.env.LINK_PREVIEW_API_KEY
+  }&q=${Helpers.normalizeUrl(url)}`
   try {
     const { data } = await axios.get(endpoint)
     return { data }
