ca8b915006feeb998ba992e82c8f00fd1ac83a3d
Narendra Syahrasyad <nanda.s@hey.com>
2020-10-05 11:30:07 -0700

    feat: add code coverage report

--- templates/vue/cypress/support/index.js
+++ templates/vue/cypress/support/index.js
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
+import "@cypress/code-coverage/support"
 import "./commands"
 import { cleanup } from "./utils"
 
