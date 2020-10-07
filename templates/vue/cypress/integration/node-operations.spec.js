2ca17ccc4ad146aca0f46c3c886a18d8bd480f30
Narendra Syahrasyad <nanda.s@hey.com>
2020-10-07 10:25:54 -0700

    implement node operation tests

--- templates/vue/cypress/integration/node-operations.spec.js
+++ templates/vue/cypress/integration/node-operations.spec.js
@@ -10,25 +10,41 @@ describe("Node Operations", () => {
     Given: A Tapestry node and its author
     When: The tapestry loads
     Then: Its add, edit, and media buttons should be visible
-  `)
+  `, () => {
+    cy.getSelectedNode().then(({ id }) => {
+      cy.getByTestId(`add-node-${id}`).should("exist")
+      cy.getByTestId(`edit-node-${id}`).should("exist")
+      cy.getByTestId(`open-node-${id}`).should("exist")
+    })
+  })
 
   it(`
     Given: A Tapestry node
     When: Its media button is clicked
     Then: Its lightbox should be visible
-  `)
+  `, () => {
+    cy.getSelectedNode()
+      .openLightbox()
+      .should("exist")
+  })
 
   it(`
     Given: A Tapestry node and its author
     When: Its add button is clicked
     Then: The add node modal should be visible
-  `)
+  `, () => {
+    cy.getSelectedNode().addNode()
+    cy.getByTestId("node-modal").should("be.visible")
+  })
 
   it(`
     Given: A Tapestry node and its author
     When: Its edit button is clicked
     Then: The edit node modal should be visible
-  `)
+  `, () => {
+    cy.getSelectedNode().editNode()
+    cy.getByTestId("node-modal").should("be.visible")
+  })
 
   it(`
     Given: A Tapestry node that does not have a media button
