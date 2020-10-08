c6b9a44fb35b2f21b751c38d9e54cf39ea1540cb
Narendra Syahrasyad <nanda.s@hey.com>
2020-10-08 12:59:42 -0700

    improve node operations tests

--- templates/vue/cypress/integration/node-operations.spec.js
+++ templates/vue/cypress/integration/node-operations.spec.js
@@ -9,41 +9,20 @@ describe("Node Operations", () => {
   it(`
     Given: A Tapestry node and its author
     When: The tapestry loads
-    Then: Its add, edit, and media buttons should be visible
+    Then: Its add, edit, and media buttons should be visible and clickable
   `, () => {
     cy.getSelectedNode().then(({ id }) => {
-      cy.getByTestId(`add-node-${id}`).should("exist")
-      cy.getByTestId(`edit-node-${id}`).should("exist")
-      cy.getByTestId(`open-node-${id}`).should("exist")
+      cy.getByTestId(`add-node-${id}`).click()
+      cy.getByTestId("node-modal").should("be.visible")
+      cy.contains(/cancel/i).click()
+
+      cy.getByTestId(`edit-node-${id}`).click()
+      cy.getByTestId("node-modal").should("be.visible")
+      cy.contains(/cancel/i).click()
+
+      cy.getByTestId(`open-node-${id}`).click()
+      cy.getByTestId("lightbox").should("be.visible")
     })
-  })
-
-  it(`
-    Given: A Tapestry node
-    When: Its media button is clicked
-    Then: Its lightbox should be visible
-  `, () => {
-    cy.getSelectedNode()
-      .openLightbox()
-      .should("exist")
-  })
-
-  it(`
-    Given: A Tapestry node and its author
-    When: Its add button is clicked
-    Then: The add node modal should be visible
-  `, () => {
-    cy.getSelectedNode().addNode()
-    cy.getByTestId("node-modal").should("be.visible")
-  })
-
-  it(`
-    Given: A Tapestry node and its author
-    When: Its edit button is clicked
-    Then: The edit node modal should be visible
-  `, () => {
-    cy.getSelectedNode().editNode()
-    cy.getByTestId("node-modal").should("be.visible")
   })
 
   it(`
@@ -51,13 +30,12 @@ describe("Node Operations", () => {
     When: The node is clicked
     Then: Its lightbox should be visible
   `, () => {
-    const newNode = {
-      hideMedia: true,
-    }
     cy.getSelectedNode().then(node => {
-      cy.wrap(node).editNodeInStore(newNode)
+      cy.wrap(node).editNodeInStore({
+        hideMedia: true,
+      })
       cy.getByTestId(`node-${node.id}`).click()
+      cy.getByTestId("lightbox").should("be.visible")
     })
-    cy.get("#lightbox").should("exist")
   })
 })
