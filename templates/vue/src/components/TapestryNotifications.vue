b2c1a69e74bb7c1364c4fe778fdc06611571e7a7
seanRong <seantxrong@gmail.com>
2020-10-09 00:35:21 -0700

    adding UI

---
+++ templates/vue/src/components/TapestryNotifications.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-dropdown id="dropdown-1" :text="text" class="m-md-2">
+      <b-dropdown-item>First Action</b-dropdown-item>
+      <b-dropdown-item>Second Action</b-dropdown-item>
+      <b-dropdown-item>Third Action</b-dropdown-item>
+      <b-dropdown-divider></b-dropdown-divider>
+      <b-dropdown-item active>Active action</b-dropdown-item>
+      <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+    </b-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tapestry-notifications",
+  data() {
+    return {
+      pending: 0,
+    }
+  },
+  computed: {
+    text() {
+      return "Notifications " + this.pending
+    },
+  },
+}
+</script>
