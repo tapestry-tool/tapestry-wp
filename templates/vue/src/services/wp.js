31cf57aceb5024cb977cf1f37b3d6b12793de148
Nanda Syahrasyad <nanda.s@hey.com>
2020-10-22 00:42:10 -0700

    766 - Add Jest-based integration test boilerplate (#767)
    * Add necessary boilerplate and configuration for Jest and Vue Testing Library to run
    * Refactor the dataset parser to a utility function that can be used by the tests
    * Move the global wpData variable to a wp module so it can be mocked out
    * Add two simple tests (DuplicateTapestryButton and Lightbox) to show how it works
    * Add GitHub Action to run these tests on every PR
    Co-authored-by: Aidin Niavarani <aidin@wynnset.com>

---
+++ templates/vue/src/services/wp.js
@@ -0,0 +1,5 @@
+export const data = {
+  ...wpData,
+  apiUrl,
+  postId: wpPostId,
+}
---
+++ templates/vue/src/utils/dataset.js
@@ -0,0 +1,84 @@
+import { DEFAULT_DEPTH } from "./constants"
+
+export function parse(dataset) {
+  const store = {
+    ...dataset,
+  }
+
+  for (const node of dataset.nodes) {
+    const { imageURL, lockedImageURL } = node
+    const { mediaURL } = node.typeData
+    if (imageURL) {
+      node.imageURL = imageURL.replace(/(http(s?)):\/\//gi, "//")
+    }
+    if (lockedImageURL) {
+      node.lockedImageURL = lockedImageURL.replace(/(http(s?)):\/\//gi, "//")
+    }
+    if (mediaURL && typeof mediaURL === "string") {
+      node.typeData.mediaURL = mediaURL.replace(/(http(s?)):\/\//gi, "//")
+    }
+  }
+
+  for (const node of dataset.nodes.filter(node => node.mediaType === "accordion")) {
+    const accordionRowIds = getChildIds({ links: dataset.links }, node.id)
+    accordionRowIds.forEach(accordionRowId => {
+      const accordionRow = getNode(dataset, accordionRowId)
+      accordionRow.presentationStyle = "accordion-row"
+      const subRows = getChildIds({ links: dataset.links }, accordionRowId)
+      if (subRows.length) {
+        accordionRow.isSubAccordion = true
+      }
+      subRows.forEach(id => {
+        const subRow = getNode(dataset, id)
+        subRow.presentationStyle = "accordion-row"
+      })
+    })
+  }
+
+  store.settings = dataset.settings || {}
+  const { defaultDepth } = store.settings
+  if (defaultDepth === undefined) {
+    store.settings.defaultDepth = DEFAULT_DEPTH
+  }
+
+  dataset.nodes
+    .filter(n => n.mediaType === "accordion" || n.isSubAccordion)
+    .forEach(n => initializeOrdering(dataset, n))
+
+  store.selectedNodeId = dataset.rootId
+  store.visibleNodes = dataset.nodes.map(node => parseInt(node.id, 10))
+  store.nodes = dataset.nodes.reduce((acc, node) => {
+    acc[node.id] = node
+    return acc
+  }, {})
+  store.links = dataset.links.filter(link => {
+    const { source, target } = link
+    return (
+      getNode(dataset, source) !== undefined &&
+      getNode(dataset, target) !== undefined
+    )
+  })
+
+  return store
+}
+
+function initializeOrdering(state, node) {
+  getChildIds(state, node.id)
+    .filter(cid => !node.childOrdering.includes(cid))
+    .forEach(id => node.childOrdering.push(id))
+  const children = getChildIds(state, node.id)
+  node.childOrdering = node.childOrdering.filter(id => children.includes(id))
+}
+
+export function getNode(dataset, nodeId) {
+  return dataset.nodes.find(node => node.id == nodeId)
+}
+
+export function getChildIds(dataset, nodeId) {
+  const links = dataset.links
+  return links
+    .filter(link =>
+      link.source.id == undefined ? link.source == nodeId : link.source.id == nodeId
+    )
+    .map(link => link.target)
+}
