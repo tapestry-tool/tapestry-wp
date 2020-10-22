66b71e4deaa60314cb774037240f88e17289537d
Narendra Syahrasyad <nanda.s@hey.com>
2020-10-22 15:11:55 -0700

    fix: update docs and fix report commands

--- templates/vue/cypress/plugins/index.js
+++ templates/vue/cypress/plugins/index.js
@@ -13,5 +13,6 @@
 
 module.exports = (on, config) => {
   require("@cypress/code-coverage/task")(on, config)
+  on("file:preprocessor", require("@cypress/code-coverage/use-browserify-istanbul"))
   return config
 }
