1a60bad824068d9ee6047792447c67cad78c3d89
Narendra Syahrasyad <nanda.s@hey.com>
2020-10-30 12:31:27 -0700

    fix: build errors

--- templates/vue/src/components/TapestryLink.vue
+++ templates/vue/src/components/TapestryLink.vue
@@ -18,7 +18,7 @@
 
 <script>
 import { mapActions, mapGetters, mapState } from "vuex"
-import { isLoggedIn } from "@/utils/wp"
+import * as wp from "@/services/wp"
 
 export default {
   name: "tapestry-link",
@@ -39,7 +39,7 @@ export default {
       return this.isVisible(this.source.id) && this.isVisible(this.target.id)
     },
     isLoggedIn() {
-      return isLoggedIn
+      return wp.isLoggedIn()
     },
   },
   methods: {
