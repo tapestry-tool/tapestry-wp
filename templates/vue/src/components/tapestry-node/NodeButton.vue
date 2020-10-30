173256f3b6df6822bb80ec2d364c131f3b820163
Narendra Syahrasyad <nanda.s@hey.com>
2020-10-30 10:53:10 -0700

    fix: buttons hard to click

--- templates/vue/src/components/tapestry-node/NodeButton.vue
+++ templates/vue/src/components/tapestry-node/NodeButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <g :transform="`translate(${x}, ${y})`">
+  <g :transform="`translate(${x}, ${y})`" @click.stop="$emit('click')">
     <circle
       ref="addButton"
       r="27"
@@ -8,7 +8,6 @@
       stroke-width="3"
       :aria-disabled="disabled"
       :data-qa="dataQa"
-      @click.stop="$emit('click')"
     ></circle>
     <slot></slot>
   </g>
