8b89d3725dac94bd6facea961d9185745c59be01
wynnset <aidin@wynnset.com>
2020-11-02 15:44:22 -0800

    Remove unused and inaccurate isError() method

--- templates/vue/src/utils/errorHelper.js
+++ templates/vue/src/utils/errorHelper.js
@@ -1,16 +1,5 @@
 export default class ErrorHelper {
   /**
-   * Return true if response is an error
-   *
-   * @param {Object} response
-   *
-   * @return {Boolean}
-   */
-  static isError(response) {
-    return response.status !== 200
-  }
-
-  /**
    * Get error message from response
    *
    * @param {Object} response
