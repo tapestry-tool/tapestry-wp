b3437dc469e2c139d217f62275550f6bb9e18c49
shirleywbi <s.bi@alumni.ubc.ca>
2020-11-02 02:12:27 -0800

    update fireEvents from change to update for inputs

--- templates/vue/src/components/__tests__/NodeModal.spec.js
+++ templates/vue/src/components/__tests__/NodeModal.spec.js
@@ -4,6 +4,7 @@ import { render } from "@/utils/test"
 
 import root from "@/fixtures/root.json"
 import NodeModal from "@/components/NodeModal.vue"
+import client from "@/services/TapestryAPI"
 import GravityFormsApi from "@/services/GravityFormsApi"
 import routes, { names } from "@/config/routes"
 
@@ -23,6 +24,13 @@ jest.mock("@/services/GravityFormsApi", () => {
     ...jest.requireActual("@/services/GravityFormsApi"),
     exists: jest.fn(),
     getAllForms: jest.fn(),
+  }
+})
+
+jest.mock("@/services/TapestryAPI", () => {
+  return {
+    ...jest.requireActual("@/services/TapestryAPI"),
+    addNode: jest.fn(),
   }
 })
 
@@ -51,43 +59,36 @@ describe("node modal", () => {
   })
 
   it("should create video node for youtube video", async () => {
+    client.addNode.mockImplementation(() => Promise.resolve(true))
     const youtubeURL = "https://youtu.be/d63DL-Erz50"
 
-    await fireEvent.change(screen.getByPlaceholderText(/title/i), {
-      target: { value: "Test Title" },
-    })
+    await fireEvent.update(screen.getByPlaceholderText(/title/i), "Test Title")
     await fireEvent.change(screen.getByTestId("node-mediaType"), {
       target: { value: "video" },
     })
     expect(screen.queryByText("Video")).toBeInTheDocument()
     expect(screen.queryByText("Video URL")).toBeInTheDocument()
 
-    await fireEvent.change(screen.getByTestId("node-videoUrl"), {
-      target: { value: youtubeURL },
-    })
+    await fireEvent.update(screen.getByTestId("node-videoUrl"), youtubeURL)
     expect(screen.getByTestId("node-videoUrl").value).toMatch(youtubeURL)
 
     await fireEvent.click(screen.getByText(/Publish/i))
     expect(
       screen.queryByText("Please enter a valid Video URL")
     ).not.toBeInTheDocument()
-    await screen.waitForElementToBeRemoved(() => screen.queryByText("Publish"))
   })
 
   it("should not create video node for invalid video url", async () => {
     const fakeURL = "www.testing.com"
-    await fireEvent.change(screen.getByPlaceholderText(/title/i), {
-      target: { value: "Test Title" },
-    })
+
+    await fireEvent.update(screen.getByPlaceholderText(/title/i), "Test Title")
     await fireEvent.change(screen.getByTestId("node-mediaType"), {
       target: { value: "video" },
     })
     expect(screen.queryByText("Video")).toBeInTheDocument()
     expect(screen.queryByText("Video URL")).toBeInTheDocument()
 
-    await fireEvent.change(screen.getByTestId("node-videoUrl"), {
-      target: { value: fakeURL },
-    })
+    await fireEvent.update(screen.getByTestId("node-videoUrl"), fakeURL)
     expect(screen.getByTestId("node-videoUrl").value).toMatch(fakeURL)
 
     await fireEvent.click(screen.getByText(/Publish/i))
