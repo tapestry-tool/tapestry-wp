410445af69131fe8463912e2abffab1d60ad9253
shirleywbi <s.bi@alumni.ubc.ca>
2020-11-02 02:20:58 -0800

    clean up repeated code

--- templates/vue/src/components/__tests__/NodeModal.spec.js
+++ templates/vue/src/components/__tests__/NodeModal.spec.js
@@ -4,7 +4,6 @@ import { render } from "@/utils/test"
 
 import root from "@/fixtures/root.json"
 import NodeModal from "@/components/NodeModal.vue"
-import client from "@/services/TapestryAPI"
 import GravityFormsApi from "@/services/GravityFormsApi"
 import routes, { names } from "@/config/routes"
 
@@ -27,13 +26,6 @@ jest.mock("@/services/GravityFormsApi", () => {
   }
 })
 
-jest.mock("@/services/TapestryAPI", () => {
-  return {
-    ...jest.requireActual("@/services/TapestryAPI"),
-    addNode: jest.fn(),
-  }
-})
-
 const node = root.nodes[0]
 const route = {
   name: names.MODAL,
@@ -49,7 +41,7 @@ const mocks = {
   $route: route,
 }
 
-describe("node modal", () => {
+describe("node modal: content - video", () => {
   let screen
 
   beforeEach(async () => {
@@ -58,10 +50,7 @@ describe("node modal", () => {
     await fireEvent.click(screen.getByText("Modal Button"))
   })
 
-  it("should create video node for youtube video", async () => {
-    client.addNode.mockImplementation(() => Promise.resolve(true))
-    const youtubeURL = "https://youtu.be/d63DL-Erz50"
-
+  async function testVideoSetup(screen, url) {
     await fireEvent.update(screen.getByPlaceholderText(/title/i), "Test Title")
     await fireEvent.change(screen.getByTestId("node-mediaType"), {
       target: { value: "video" },
@@ -69,10 +58,15 @@ describe("node modal", () => {
     expect(screen.queryByText("Video")).toBeInTheDocument()
     expect(screen.queryByText("Video URL")).toBeInTheDocument()
 
-    await fireEvent.update(screen.getByTestId("node-videoUrl"), youtubeURL)
-    expect(screen.getByTestId("node-videoUrl").value).toMatch(youtubeURL)
+    await fireEvent.update(screen.getByTestId("node-videoUrl"), url)
+    expect(screen.getByTestId("node-videoUrl").value).toMatch(url)
 
     await fireEvent.click(screen.getByText(/Publish/i))
+  }
+
+  it("should create video node for youtube video", async () => {
+    const youtubeURL = "https://youtu.be/d63DL-Erz50"
+    await testVideoSetup(screen, youtubeURL)
     expect(
       screen.queryByText("Please enter a valid Video URL")
     ).not.toBeInTheDocument()
@@ -80,18 +74,7 @@ describe("node modal", () => {
 
   it("should not create video node for invalid video url", async () => {
     const fakeURL = "www.testing.com"
-
-    await fireEvent.update(screen.getByPlaceholderText(/title/i), "Test Title")
-    await fireEvent.change(screen.getByTestId("node-mediaType"), {
-      target: { value: "video" },
-    })
-    expect(screen.queryByText("Video")).toBeInTheDocument()
-    expect(screen.queryByText("Video URL")).toBeInTheDocument()
-
-    await fireEvent.update(screen.getByTestId("node-videoUrl"), fakeURL)
-    expect(screen.getByTestId("node-videoUrl").value).toMatch(fakeURL)
-
-    await fireEvent.click(screen.getByText(/Publish/i))
+    await testVideoSetup(screen, fakeURL)
     expect(screen.queryByText("Please enter a valid Video URL")).toBeInTheDocument()
   })
 })
