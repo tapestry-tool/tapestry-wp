8937750a7e00e954248f3522a1275ac2a73d8765
Narendra Syahrasyad <nanda.s@hey.com>
2020-11-05 13:32:07 -0800

    test: add brief description to spec

--- templates/vue/cypress/integration/search-bar.spec.js
+++ templates/vue/cypress/integration/search-bar.spec.js
@@ -1,10 +1,15 @@
 describe("Search bar", () => {
-  beforeEach(() => {
+  /* beforeEach(() => {
     cy.fixture("multi-author.json").as("tapestry")
     cy.setup("@tapestry")
-  })
+  }) */
 
-  it("should be able to search nodes by its title")
+  it("should be able to search nodes by its title", () => {
+    // click search button
+    // assert search by title by default
+    // type in 'hello'
+    // assert correct nodes are visible
+  })
 
   it("should be able to search nodes by author")
 
