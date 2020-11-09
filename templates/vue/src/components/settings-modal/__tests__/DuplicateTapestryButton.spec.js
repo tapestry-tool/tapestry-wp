1b5618c4a555150df52951a906f510be934ecd9f
shirleywbi <s.bi@alumni.ubc.ca>
2020-11-09 11:07:03 -0800

    fix root file reference in jest tests

--- templates/vue/src/components/settings-modal/__tests__/DuplicateTapestryButton.spec.js
+++ templates/vue/src/components/settings-modal/__tests__/DuplicateTapestryButton.spec.js
@@ -19,7 +19,7 @@ describe("duplicate tapestry button", () => {
       Promise.resolve({ settings: { permalink: slug } })
     )
 
-    const screen = render(DuplicateTapestryButton, { fixture: root })
+    const screen = render(DuplicateTapestryButton, { fixture: oneNodeTapestry })
     fireEvent.click(screen.getByText(/duplicate tapestry/i))
 
     expect(client.addTapestry).toHaveBeenCalledTimes(1)
