b4b6da31c92d45e83b7603ce9c9ed11ece62700d
Narendra Syahrasyad <nanda.s@hey.com>
2020-11-09 13:55:55 -0800

    test(e2e): add cypress tests

--- templates/vue/cypress/integration/search-bar.spec.js
+++ templates/vue/cypress/integration/search-bar.spec.js
@@ -1,17 +1,51 @@
+const assertVisibleNodes = visibleNodes => {
+  cy.store()
+    .its("state.nodes")
+    .then(nodes => {
+      for (const node of Object.values(nodes)) {
+        cy.getNodeById(node.id)
+          .should("have.css", "opacity")
+          .and(opacity => {
+            const opacityAsNum = Number(opacity)
+            if (visibleNodes.includes(node.id)) {
+              expect(opacityAsNum).to.equal(1)
+            } else {
+              expect(opacityAsNum).to.be.lessThan(1)
+            }
+          })
+      }
+    })
+}
+
 describe("Search bar", () => {
-  /* beforeEach(() => {
+  beforeEach(() => {
     cy.fixture("multi-author.json").as("tapestry")
     cy.setup("@tapestry")
-  }) */
-
-  it("should be able to search nodes by its title", () => {
-    // click search button
-    // assert search by title by default
-    // type in 'hello'
-    // assert correct nodes are visible
   })
 
-  it("should be able to search nodes by author")
+  it("should be able to search nodes via different attributes", () => {
+    cy.store()
+      .its("state.nodes")
+      .then(nodes => {
+        const allNodes = Object.values(nodes)
+        assertVisibleNodes(allNodes.map(node => node.id))
 
-  it("should be able to search nodes by published status")
+        cy.get("[aria-label=search]").click()
+        assertVisibleNodes([])
+
+        // By title
+        cy.findByPlaceholderText("Node title").type("first")
+        let expected = allNodes.filter(node => node.title.startsWith("first"))
+        assertVisibleNodes(expected.map(node => node.id))
+
+        // By author
+        cy.findByDisplayValue("Title").select("Author")
+        cy.findByPlaceholderText("Node author").type("admin")
+        expected = Object.values(nodes).filter(node => node.author.name === "admin")
+        assertVisibleNodes(expected.map(node => node.id))
+
+        // By status
+        cy.findByDisplayValue("Author").select("Status")
+      })
+  })
 })
