6ea091807c464917ad3b905c171c11ad2df34749
Narendra Syahrasyad <nanda.s@hey.com>
2020-11-20 13:22:29 -0800

    test: add scroll test

--- templates/vue/cypress/integration/sidebar.spec.js
+++ templates/vue/cypress/integration/sidebar.spec.js
@@ -7,7 +7,14 @@ describe("Sidebar", () => {
       .then(id => {
         cy.editNode(id, {
           license: {
-            type: "cc",
+            type: "custom",
+            description: `Copyright <YEAR> <COPYRIGHT HOLDER>
+
+            Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+            
+            The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+            
+            THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`,
           },
         })
       })
@@ -28,6 +35,9 @@ describe("Sidebar", () => {
 
     cy.findByLabelText("copyright").click()
     assertActive("copyright")
+
+    cy.getByTestId("sidebar-content").scrollTo(0, 0)
+    assertActive("information")
 
     cy.findByLabelText("close sidebar").click()
     cy.getByTestId("sidebar-content").should("not.be.visible")
