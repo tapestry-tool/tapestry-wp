be0e3d28ab615d18e96cbd98595d05b8445151c0
Narendra Syahrasyad <nanda.s@hey.com>
2020-12-08 12:24:54 -0800

    feat: update review text

--- templates/vue/src/components/node-review/review-log/StatusChangeEvent.vue
+++ templates/vue/src/components/node-review/review-log/StatusChangeEvent.vue
@@ -1,7 +1,7 @@
 <template>
   <p>
     <span>{{ event.author.name }}</span>
-    changed status from {{ event.from }} to {{ event.to }}.
+    {{ text }}
     <span class="timestamp">{{ event.time }}</span>
   </p>
 </template>
@@ -15,11 +15,28 @@
  * - `from` the original status of the node
  * - `to` the new status of the node
  */
+
+import { nodeStatus } from "@/utils/constants"
+
 export default {
   props: {
     event: {
       type: Object,
       required: true,
+    },
+  },
+  computed: {
+    text() {
+      switch (this.event.to) {
+        case nodeStatus.SUBMIT:
+          return "submitted this node for review."
+        case nodeStatus.REJECT:
+          return "rejected this node."
+        case nodeStatus.ACCEPT:
+          return "accepted this node."
+        default:
+          return `changed status from ${this.event.from} to ${this.event.to}.`
+      }
     },
   },
 }
