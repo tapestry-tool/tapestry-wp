bd427bbafc29176f3e68664bfbe1e5efc013502d
shirleywbi <s.bi@alumni.ubc.ca>
2021-02-05 17:13:23 -0800

    add warning modal if there are draft nodes

--- templates/vue/src/components/modals/NodeModal/DeleteNodeButton.vue
+++ templates/vue/src/components/modals/NodeModal/DeleteNodeButton.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <b-button :disabled="isDisabled" size="sm" variant="danger" @click="removeNode">
+    <b-button
+      :disabled="isDisabled"
+      size="sm"
+      variant="danger"
+      @click="handleRemoveNode"
+    >
       Delete Node
     </b-button>
   </div>
@@ -56,11 +61,35 @@ export default {
     this.$emit("message", this.disabledMessage)
   },
   methods: {
-    ...mapActions(["deleteNode", "deleteLink"]),
+    ...mapActions(["deleteNode", "deleteLink", "getNodeHasDraftNeighbours"]),
     ...mapMutations(["updateSelectedNode", "updateNode"]),
-    removeNode() {
-      this.$emit("submit")
+    async handleRemoveNode() {
+      this.$emit("setLoading", true)
       this.updateSelectedNode(this.rootId)
+      const hasDraftNeighbours = await this.getNodeHasDraftNeighbours(this.nodeId)
+      if (hasDraftNeighbours) {
+        this.$bvModal
+          .msgBoxConfirm(
+            "There are draft nodes attached to this node. Deleting this node will also remove the draft nodes. Are you sure you want to continue?",
+            {
+              modalClass: "node-modal-confirmation",
+              title: "Are you sure you want to continue?",
+              okTitle: "Yes, Delete!",
+              okVariant: "danger",
+            }
+          )
+          .then(close => {
+            if (close) {
+              this.removeNode()
+            }
+          })
+          .catch(err => console.log(err))
+          .finally(() => this.$emit("setLoading", false))
+      } else {
+        this.removeNode()
+      }
+    },
+    removeNode() {
       if (!this.isRoot) {
         this.deleteLink({
           source: this.neighbourLink.source,
