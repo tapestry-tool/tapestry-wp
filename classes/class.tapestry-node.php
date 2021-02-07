3a2f2dd95bb703d25ee7426b64dcace62630dd86
jaskwong <52149923+jaskwong@users.noreply.github.com>
2021-02-06 19:10:32 -0700

    765 - Bug Fix: Old H5P Nodes, When Edited, Show Up as "Video" Content Type (#947)
    * Creates a new tapestry-settings.php file for the tapestry settings page with one database command to clean old h5p nodes.
    * Filters nodes on loading to ensure h5p media formats and the media type are compatible 
    * Adds a setting page for tapestry
    * Add a button to run cleanup of tapestries to fix old H5P nodes
    
    Co-authored-by: Shirley W Bi <45089842+shirleywbi@users.noreply.github.com>
    Co-authored-by: Aidin Niavarani <aidin@wynnset.com>

--- classes/class.tapestry-node.php
+++ classes/class.tapestry-node.php
@@ -538,6 +538,9 @@ class TapestryNode implements ITapestryNode
 
     private function _formNode()
     {
+        if ($this->mediaType == "video" && $this->mediaFormat == "h5p") {
+            $this->mediaType = "h5p";
+        }
         return (object) [
             'id' => $this->nodeMetaId,
             'postId' => $this->nodePostId,
