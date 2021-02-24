e9c0ef54066d5aea0bf1209a36704e0f7bc6e56f
shirleywbi <s.bi@alumni.ubc.ca>
2021-02-24 10:07:17 -0800

    add subpage

---
+++ templates/vue/src/components/Lightbox/media/MultiContentMedia/SubPage.vue
@@ -0,0 +1,119 @@
+<template>
+  <div ref="container">
+    <div
+      v-for="(row, index) in rows"
+      ref="rowRefs"
+      :key="row.id"
+      class="sub-page-row"
+    >
+      <div class="title-row">
+        <a class="title-row-icon" @click="toggleFavourite(row.id)">
+          <i
+            v-if="isFavourite(row.id)"
+            class="fas fa-heart fa-sm"
+            style="color:red;"
+          ></i>
+          <i v-else class="fas fa-heart fa-sm" style="color:white;"></i>
+        </a>
+      </div>
+      <tapestry-media
+        :node-id="row.id"
+        :dimensions="dimensions"
+        context="accordion"
+        :autoplay="false"
+        @complete="completeNode(row.id)"
+        @load="handleLoad(index)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex"
+import TapestryMedia from "../TapestryMedia"
+import { names } from "@/config/routes"
+
+export default {
+  name: "sub-page",
+  components: {
+    TapestryMedia,
+  },
+  props: {
+    dimensions: {
+      type: Object,
+      required: false,
+      default: () => ({}),
+    },
+    rows: {
+      type: Array,
+      required: true,
+    },
+    rowId: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapGetters(["isFavourite"]),
+  },
+  methods: {
+    ...mapActions(["completeNode", "toggleFavourite"]),
+    handleLoad(idx) {
+      this.$emit("load", this.$refs.rowRefs[idx])
+    },
+    changeRow(subRowId) {
+      const { nodeId, rowId } = this.$route.params
+      if (subRowId) {
+        this.$router.push({
+          name: names.SUBACCORDION,
+          params: { nodeId, rowId, subRowId },
+          query: this.$route.query,
+        })
+      } else {
+        this.$router.push({
+          name: names.ACCORDION,
+          params: { nodeId, rowId },
+          query: this.$route.query,
+        })
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.title-row {
+  display: flex;
+  align-items: center;
+  margin: 0;
+  width: 100%;
+  border-radius: 4px;
+}
+
+.title-row-icon {
+  flex: 1;
+  text-align: right;
+
+  a {
+    cursor: pointer;
+  }
+}
+
+.button-row-trigger {
+  background: none;
+  width: 100%;
+  text-align: left;
+}
+
+.sub-page-row {
+  background: rgb(30, 30, 30);
+  border-radius: 4px;
+  padding: 8px 16px;
+  margin-bottom: 8px;
+  color: white;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+</style>
