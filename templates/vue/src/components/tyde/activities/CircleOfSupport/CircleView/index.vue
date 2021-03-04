242b7711d8da33e73c821ffef328bd991bdb2cf3
Narendra Syahrasyad <nanda.s@hey.com>
2021-03-04 14:10:57 -0800

    dev: add debug controls

--- templates/vue/src/components/tyde/activities/CircleOfSupport/CircleView/index.vue
+++ templates/vue/src/components/tyde/activities/CircleOfSupport/CircleView/index.vue
@@ -9,11 +9,26 @@
         '--order': circle.order,
       }"
       class="circle"
-    ></li>
+    >
+      <ul>
+        <li v-for="connection in circle.connections" :key="connection.id">
+          {{ connection.avatar }}
+        </li>
+      </ul>
+    </li>
+    <div class="controls">
+      <p>Circle: {{ activeCircle }}</p>
+      <button @click="activeCircle++">+</button>
+      <button @click="activeCircle--">-</button>
+      <button @click="addConnection">Add connection</button>
+      <button @click="removeConnection">Remove connection</button>
+    </div>
   </ul>
 </template>
 
 <script>
+import Helpers from "@/utils/Helpers"
+
 export default {
   props: {
     communities: {
@@ -24,18 +39,39 @@ export default {
       type: Object,
       required: true,
     },
-    circles: {
-      type: Array,
-      required: true,
-    },
+  },
+  // STUB: Change this to props
+  data() {
+    return {
+      circles: [[], [], []],
+      activeCircle: 0,
+    }
   },
   computed: {
     circlesWithData() {
       return this.circles.map((connections, index) => {
         const order = this.circles.length - index
         const borderWidth = `${order}px`
-        return { borderWidth, connections, order }
+        return {
+          borderWidth,
+          connections,
+          order,
+        }
       })
+    },
+  },
+  methods: {
+    addConnection() {
+      const connection = {
+        id: Helpers.createUUID(),
+        name: "nan",
+        avatar: "😀",
+        communities: ["603e883568aae", "603e884ee69c1"],
+      }
+      this.circles[this.activeCircle].push(connection)
+    },
+    removeConnection() {
+      this.circles[this.activeCircle].pop()
     },
   },
 }
@@ -63,5 +99,17 @@ export default {
   height: var(--radius);
   border-radius: 50%;
   z-index: calc(var(--order) * 10);
+}
+
+.controls {
+  position: absolute;
+  right: 2rem;
+  bottom: 2rem;
+}
+
+button {
+  display: block;
+  width: 100%;
+  font-size: 0.8em;
 }
 </style>
