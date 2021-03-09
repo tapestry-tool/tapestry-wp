b5de5eb892983b19965b10639a95b5c599cd24f7
Narendra Syahrasyad <nanda.s@hey.com>
2021-03-09 15:05:44 -0800

    refactor: move connection tooltip to own component

--- templates/vue/src/components/tyde/activities/CircleOfSupport/CommunityView/CommunitiesList.vue
+++ templates/vue/src/components/tyde/activities/CircleOfSupport/CommunityView/CommunitiesList.vue
@@ -59,32 +59,13 @@
         </ul>
       </div>
     </li>
-    <div
+    <connection-tooltip
       ref="connection-tooltip"
-      :class="['connection-tooltip', { active: isTooltipVisible }]"
-    >
-      <div class="info">
-        <p>{{ activeConnection.name }}</p>
-        <h1>
-          {{ activeConnection.avatar }}
-        </h1>
-        <ul class="community-list">
-          <li
-            v-for="community in activeConnection.communities"
-            :key="community.id"
-            :style="`--community-color: ${community.color}`"
-          ></li>
-        </ul>
-      </div>
-      <div class="controls">
-        <button @click="editConnection">
-          <tapestry-icon icon="pencil-alt" />
-        </button>
-        <button @click="activeConnectionId = null">
-          <tapestry-icon icon="times" />
-        </button>
-      </div>
-    </div>
+      :connection="activeConnection"
+      :show="isTooltipVisible"
+      @edit="editConnection"
+      @close="activeConnectionId = null"
+    />
     <div
       v-show="isCommunityTooltipVisible"
       ref="community-tooltip"
@@ -108,8 +89,11 @@
 import TapestryIcon from "@/components/common/TapestryIcon"
 import Helpers from "@/utils/Helpers"
 
+import ConnectionTooltip from "../ConnectionTooltip"
+
 export default {
   components: {
+    ConnectionTooltip,
     TapestryIcon,
   },
   props: {
@@ -226,7 +210,7 @@ export default {
       this.activeCommunityTooltipId = null
 
       const [connectionRef] = this.$refs[`${connectionId}-${communityId}`]
-      const tooltipRef = this.$refs["connection-tooltip"]
+      const tooltipRef = this.$refs["connection-tooltip"].$el
       this.$nextTick(() => this.positionTooltip(connectionRef, tooltipRef))
     },
     /**
@@ -690,83 +674,6 @@ ul {
 
   h1 {
     cursor: default;
-  }
-}
-
-.connection-tooltip {
-  position: absolute;
-  min-width: 10rem;
-  border: var(--cos-border);
-  border-radius: 1.5rem;
-  padding: 1rem 0.5rem;
-  display: flex;
-  background: white;
-  z-index: 20;
-  opacity: 0;
-  pointer-events: none;
-
-  &.active {
-    opacity: 1;
-    pointer-events: all;
-  }
-
-  button {
-    color: inherit;
-    background: none;
-    padding: 0;
-    margin: 0;
-    font-size: 1.5em;
-
-    &:last-child {
-      margin-top: 1rem;
-    }
-  }
-
-  .info {
-    flex: 3;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-right: 0.5rem;
-
-    p {
-      padding: 0.25rem;
-      border: 1px solid black;
-      text-transform: uppercase;
-      color: black;
-      cursor: default;
-      font-size: 0.7em;
-      margin: 0;
-    }
-
-    h1 {
-      font-size: 4em;
-    }
-  }
-
-  .controls {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    color: var(--cos-color-secondary);
-  }
-
-  .community-list {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    display: flex;
-    justify-content: center;
-    column-gap: 4px;
-
-    li {
-      height: 1rem;
-      width: 1rem;
-      border-radius: 50%;
-      background-color: var(--community-color);
-    }
   }
 }
 
