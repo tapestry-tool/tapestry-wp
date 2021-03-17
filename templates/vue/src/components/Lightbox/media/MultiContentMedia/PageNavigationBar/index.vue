2df9c9d6858be7cc970372d2a01246a273f729c4
shirleywbi <s.bi@alumni.ubc.ca>
2021-03-17 12:17:43 -0700

    update route if scroll

--- templates/vue/src/components/Lightbox/media/MultiContentMedia/PageNavigationBar/index.vue
+++ templates/vue/src/components/Lightbox/media/MultiContentMedia/PageNavigationBar/index.vue
@@ -38,6 +38,10 @@ export default {
       type: Object,
       required: true,
     },
+    parentRefs: {
+      type: Object,
+      required: true,
+    },
   },
   computed: {
     ...mapGetters(["getDirectChildren", "getNode", "isMultiContent"]),
@@ -77,6 +81,9 @@ export default {
     disabledFrom() {
       return this.rows.findIndex(row => !row.node.completed)
     },
+    rowOrder() {
+      return this.getRowOrder(this.node)
+    },
   },
   watch: {
     closed: {
@@ -114,11 +121,14 @@ export default {
     },
   },
   mounted() {
-    const observer = new IntersectionObserver(this.handleObserve, {
-      threshold: [0.5, 0.8],
-    })
-    for (const ref of this.$parent.$refs.rowRefs) {
-      observer.observe(ref)
+    // TODO: Get ALL the refs
+    if (this.parentRefs.rowRefs) {
+      const observer = new IntersectionObserver(this.handleObserve, {
+        threshold: [0.5, 0.8],
+      })
+      for (const ref of this.parentRefs.rowRefs) {
+        observer.observe(ref)
+      }
     }
   },
   methods: {
@@ -135,19 +145,18 @@ export default {
       const inactive = entries.find(entry => !entry.isIntersecting)
       const nextActive = entries.find(entry => entry.intersectionRatio > 0.8)
       if (nextActive) {
-        this.active = nextActive.target.dataset.name
+        this.active = Number(nextActive.target.id.split("-")[1])
       } else if (inactive) {
-        const { name } = inactive.target.dataset
-        if (name === this.active) {
+        const nodeId = Number(inactive.target.id.split("-")[1])
+        if (nodeId == this.active) {
           this.active = this.nextTab()
         }
       }
     },
     nextTab() {
-      const rowOrder = [13048, 13050, 13052] // TODO: Get actual row values
-      const nextTabIndex = rowOrder.indexOf(this.active)
-      if (nextTabIndex >= 0 && nextTabIndex < rowOrder.length - 1) {
-        return rowOrder[nextTabIndex + 1]
+      const nextTabIndex = this.rowOrder.indexOf(this.active)
+      if (nextTabIndex >= 0 && nextTabIndex < this.rowOrder.length - 1) {
+        return this.rowOrder[nextTabIndex + 1]
       }
       return this.active
     },
@@ -155,10 +164,8 @@ export default {
       if (nodeId) {
         this.active = nodeId
         this.$nextTick(() => {
-          let el = this.$parent.$parent.$refs.rowRefs.find(
-            ref => ref.id === `row-${nodeId}`
-          )
-          if (el.hasOwnProperty("$el")) {
+          let el = this.parentRefs.rowRefs.find(ref => ref.id === `row-${nodeId}`)
+          if (el && el.hasOwnProperty("$el")) {
             el = el.$el
           }
           if (el) {
@@ -172,6 +179,19 @@ export default {
     disableRow(nodeId) {
       const index = this.rows.findIndex(row => row.node.id === nodeId)
       return this.lockRows && this.disabledFrom >= 0 && index > this.disabledFrom
+    },
+    getRowOrder(node, nodes = [], visited = new Set()) {
+      nodes.push(node.id)
+      visited.add(node.id)
+      const rows = this.isMultiContent(node.id)
+        ? node.childOrdering.map(this.getNode)
+        : this.getDirectChildren(node.id).map(this.getNode)
+      for (const row of rows) {
+        if (!visited.has(row.id)) {
+          this.getRowOrder(row, nodes, visited)
+        }
+      }
+      return nodes
     },
   },
 }
