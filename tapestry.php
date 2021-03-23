fa7db1358cda164006697fc0efb5c2beeef9a3b2
shirleywbi <s.bi@alumni.ubc.ca>
2021-03-22 20:09:21 -0700

    refactor tapestry plugin updates to separate file

--- tapestry.php
+++ tapestry.php
@@ -22,6 +22,7 @@ $TAPESTRY_USE_DEV_MODE = true;
  */
 require_once dirname(__FILE__).'/endpoints.php';
 require_once dirname(__FILE__).'/tapestry-settings.php';
+require_once dirname(__FILE__).'/tapestry-plugin-updates.php';
 
 /**
  * Register Tapestry type on initialization.
@@ -385,41 +386,3 @@ function tapestry_tool_log_event() {
 
     wp_die();
 }
-
-// Plugin Update
-
-function accordion_update() {
-    $query_args = array(
-        'post_type' => 'tapestry_node',
-        'meta_query' => array(
-            array(
-               'key'     => 'tapestry_node_data',
-               'value'   => '"mediaType";s:9:"accordion"',
-               'compare' => 'LIKE',
-            ),
-        ),
-    );
-    $query = new WP_Query( $query_args );
-    while ( $query->have_posts() ) {
-        $query->the_post();
-        $post_id = get_the_ID();
-        $node_meta_data = get_post_meta($post_id,'tapestry_node_data', true);
-        if (isset($node_meta_data->mediaType) && $node_meta_data->mediaType == 'accordion') {
-            $node_meta_data->mediaType = 'multi-content';
-            $node_meta_data->presentationStyle = 'accordion';
-            update_post_meta( $post_id, 'tapestry_node_data',  $node_meta_data);
-        }
-    }
-}
-register_activation_hook( __FILE__, 'accordion_update' );
-
-function tapestry_plugin_update() {
-    global $TAPESTRY_VERSION_NUMBER;
-    $installed_version = get_site_option('tapestry_version');
-    if ( $installed_version != $TAPESTRY_VERSION_NUMBER) {
-        accordion_update();
-        require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-        update_option( 'tapestry_version', $TAPESTRY_VERSION_NUMBER );
-    }
-}
-add_action( 'init', 'tapestry_plugin_update');
