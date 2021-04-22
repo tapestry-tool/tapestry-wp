a73940450ab3c9cda803e4bf8186952b5355034d
Nanda Syahrasyad <nanda.s@hey.com>
2021-04-22 12:25:50 -0700

    fix: incorrect time type
    Co-authored-by: Shirley W Bi <45089842+shirleywbi@users.noreply.github.com>

--- templates/vue/src/components/modals/NodeModal/index.vue
+++ templates/vue/src/components/modals/NodeModal/index.vue
@@ -845,7 +845,7 @@ export default {
 
       if (this.node.popup) {
         const { time } = this.node.popup
-        if (time == undefined) {
+        if (time === "") {
           errMsgs.push(`Please enter a time for your popup.`)
         } else if (time <= 0) {
           errMsgs.push(`Please enter a time greater than 0.`)
