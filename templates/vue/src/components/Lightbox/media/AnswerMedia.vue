cf576858bfed7df7501428b1377d98fb5c7995a4
Enid <edotadot2@gmail.com>
2021-05-21 13:54:53 -0700

    answerMedia displays all answers

--- templates/vue/src/components/Lightbox/media/AnswerMedia.vue
+++ templates/vue/src/components/Lightbox/media/AnswerMedia.vue
@@ -1,32 +1,39 @@
 <template>
   <div>
-    <activity-screen
+    <h1>Answers</h1>
+    <!-- <activity-screen
       :id="node.id"
       style="position: relative;"
       @submit="handleSubmit"
       @back="$emit('close')"
       @close="$emit('close')"
-    />
-    <h3 class="mb-4">{{ question.followUpText }}</h3>
-    <tapestry-activity
-      :key="answer.type"
-      :type="answer.type"
-      :entry="answer.entry"
-    ></tapestry-activity>
-    <div>
+    /> -->
+    <div v-if="answers.length" class="answer-container mx-auto mb-3">
+      <h3 class="mb-4">{{ question.followUpText }}</h3>
+      <tapestry-activity
+        v-for="answer in answers"
+        :key="answer.type"
+        :type="answer.type"
+        :entry="answer.entry"
+      ></tapestry-activity>
+    </div>
+    <div v-else>
       <p>You haven't done the previous activity yet.</p>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex"
-import ActivityScreen from "./common/ActivityScreen"
+import { mapGetters, mapActions } from "vuex"
+import TapestryActivity from "./common/ActivityScreen/TapestryActivity"
+// import ActivityScreen from "./common/ActivityScreen"
+// import Question from "./Question"
 
 export default {
   name: "answer-media",
   components: {
-    ActivityScreen,
+    TapestryActivity,
+    // ActivityScreen,
   },
   props: {
     node: {
@@ -41,14 +48,40 @@ export default {
       return this.node.quiz
     },
     question() {
+      console.log(this.node.quiz[0])
       return this.node.quiz[0]
     },
-    answer() {
-      return this.question.answers
+    // answers() {
+    //   console.log(this.question.answers)
+    //   let answerArray = []
+    //   answerArray.push(this.question.answers)
+    //   return answerArray
+    // },
+    answers() {
+      const answeredTypes = Object.entries(this.question.answers)
+        .filter(entry => entry[1] && entry[1].length > 0)
+        .map(i => i[0])
+      console.log(answeredTypes)
+      return answeredTypes
+        .map(type => this.getEntry(this.question.previousEntry, type))
+        .filter(Boolean)
     },
   },
   mounted() {
     this.$emit("load")
+  },
+  methods: {
+    ...mapActions(["updateNodeProgress"]),
+    handleSubmit() {
+      const numberCompleted = this.node.quiz.filter(question => question.completed)
+        .length
+      const progress = numberCompleted / this.node.quiz.length
+      this.updateNodeProgress({ id: this.node.id, progress }).then(() => {
+        if (progress === 1) {
+          this.$emit("complete")
+        }
+      })
+    },
   },
 }
 </script>
