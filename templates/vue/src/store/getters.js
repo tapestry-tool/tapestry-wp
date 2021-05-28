1e8e6461424431a3d0c32c6156f81752fd598cfe
Enid <edotadot2@gmail.com>
2021-05-28 16:24:54 -0700

    modified getEntry for to include list answers

--- templates/vue/src/store/getters.js
+++ templates/vue/src/store/getters.js
@@ -136,6 +136,13 @@ export function getEntry(_, { getQuestion }) {
       }
     }
 
+    if (mediaType === "question" && answerType === "listId") {
+      return {
+        type: "list",
+        entry: Object.values(entry)[0],
+      }
+    }
+
     const answers = getAnswersFromEntry(entry)
     return formatEntry(answers, answerType)
   }
