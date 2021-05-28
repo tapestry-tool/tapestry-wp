e6752a206b472dce8becb66dbd96eacf0be2a743
Enid <edotadot2@gmail.com>
2021-05-28 15:32:58 -0700

    progress on the styling

--- templates/vue/src/components/Lightbox/media/common/ListForm.vue
+++ templates/vue/src/components/Lightbox/media/common/ListForm.vue
@@ -1,39 +1,47 @@
 <template>
   <b-form>
-    <header>Answers</header>
-    <ul>
-      <ul v-for="(answer, index) in answerList" :key="answer.index">
-        <button @click="deleteAnswer(index)">X</button>
-        {{
-          answer
-        }}
-      </ul>
-    </ul>
-    <b-form-input
-      v-model="listAnswer"
-      :placeholder="
-        node.typeData.options.list.placeholder
-          ? node.typeData.options.list.placeholder
-          : 'Enter text and press Enter'
-      "
-      @keypress.enter="addAnswer"
-    ></b-form-input>
-    <button @click="addAnswer">Add</button>
-    <b-form-invalid-feedback :state="isAnswerValid">
-      Please enter a response.
-    </b-form-invalid-feedback>
-    <b-form-invalid-feedback :state="isSubmitValid">
-      Please enter at least one answer.
-    </b-form-invalid-feedback>
+    <div class="container">
+      <div class="list">
+        <ul
+          v-for="(answer, index) in answerList"
+          :key="answer.index"
+          class="answerItem"
+        >
+          {{
+            answer
+          }}
+          <button class="delete-button" @click="deleteAnswer(index)">
+            Delete
+          </button>
+        </ul>
+      </div>
+      <b-form-input
+        v-model="listAnswer"
+        class="textInput"
+        :placeholder="
+          node.typeData.options.list.placeholder
+            ? node.typeData.options.list.placeholder
+            : 'Enter text and press Enter'
+        "
+        @keypress.enter="addAnswer"
+      ></b-form-input>
+      <button class="add-button" @click="addAnswer">Add</button>
+      <b-form-invalid-feedback :state="isAnswerValid">
+        Please enter a response.
+      </b-form-invalid-feedback>
+      <b-form-invalid-feedback :state="isSubmitValid">
+        Please enter at least one answer.
+      </b-form-invalid-feedback>
 
-    <b-button
-      v-if="node.mediaType === 'question'"
-      class="submit-btn mt-3"
-      variant="primary"
-      @click="handleListSubmit"
-    >
-      Submit
-    </b-button>
+      <b-button
+        v-if="node.mediaType === 'question'"
+        class="submit-btn mt-3"
+        variant="primary"
+        @click="handleListSubmit"
+      >
+        Submit
+      </b-button>
+    </div>
   </b-form>
 </template>
 
@@ -109,7 +117,48 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container {
+  justify-content: center;
+  align-items: center;
+}
+.list {
+  position: relative;
+  background-color: gray;
+}
+.list ul {
+  position: relative;
+  padding: 20px;
+}
+
+.textInput {
+  margin-right: 30px;
+}
 .submit-btn {
   float: right;
+}
+.add-button {
+  margin-top: 10px;
+  margin-left: 30px;
+  float: right;
+}
+.delete-button {
+  float: right;
+  background-color: #f44336;
+  height: 30px;
+  width: 100px;
+  text-align: center;
+  display: block;
+  border-radius: 5px;
+  padding-bottom: 20px;
+  display: flex;
+  margin: auto;
+}
+
+.answerItem {
+  border-style: solid;
+  border-radius: 6px;
+  margin: 5px;
+  padding: 10px 20px;
+  border-left: 5px solid green;
 }
 </style>
