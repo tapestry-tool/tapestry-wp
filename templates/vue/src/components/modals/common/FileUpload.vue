98d32e299eafc635f804824227c17260ebad8e6f
Frozemint <nami5504@gmail.com>
2021-06-01 16:16:59 -0700

    1020 - Bug fix: Thumbnail not updating properly (#1023)
    * Add a delay before re-enabling the publish button after successfully uploading a new thumbnail
    * Explanation: Before this, "input" and "fileID" were emitted to prompt an update of node thumbnail states after 800ms, yet isUploading was being emitted as false immediately. This means it was possible to click publish before the underlying state had finished changing. By adding a further delay of 400ms (1200 - 800 = 400), we ensure all changes are properly applied before the user can click publish.
    
    Co-authored-by: Aidin Niavarani <aidin@wynnset.com>

--- templates/vue/src/components/modals/common/FileUpload.vue
+++ templates/vue/src/components/modals/common/FileUpload.vue
@@ -257,7 +257,7 @@ export default {
           if (this.isUploading) {
             this.isUploading = false
             this.confirmedUpload = false
-            this.$emit("isUploading", this.isUploading)
+            setTimeout(() => {this.$emit("isUploading", this.isUploading)}, 1200)
           }
           clearInterval(this.uploadBarInterval)
         })
