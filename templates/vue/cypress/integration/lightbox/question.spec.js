efdaee85fcd14341ec927bbd049b38ea88885004
Harry <harry@gameofapps.org>
2021-06-02 23:01:37 -0700

    "linted question"

--- templates/vue/cypress/integration/lightbox/question.spec.js
+++ templates/vue/cypress/integration/lightbox/question.spec.js
@@ -16,9 +16,18 @@ describe("Question", () => {
       cy.focused().type(question)
       cy.getByTestId("question-answer-multiple-choice").click({ force: true })
       cy.getByTestId("question-answer-multipleChoice-single").click({ force: true })
-      cy.getByTestId(`choicerow-radio-50`).getByTestId(`choicerow-radio-input-50`).click().type("100")
-      cy.getByTestId(`choicerow-radio-51`).getByTestId(`choicerow-radio-input-51`).click().type("90")
-      cy.getByTestId(`choicerow-radio-52`).getByTestId(`choicerow-radio-input-52`).click().type("10")
+      cy.getByTestId(`choicerow-radio-50`)
+        .getByTestId(`choicerow-radio-input-50`)
+        .click()
+        .type("100")
+      cy.getByTestId(`choicerow-radio-51`)
+        .getByTestId(`choicerow-radio-input-51`)
+        .click()
+        .type("90")
+      cy.getByTestId(`choicerow-radio-52`)
+        .getByTestId(`choicerow-radio-input-52`)
+        .click()
+        .type("10")
 
       cy.submitModal()
       cy.openLightbox(node.id)
@@ -26,17 +35,19 @@ describe("Question", () => {
       cy.route("POST", "**/quiz*").as("submit")
 
       cy.lightbox().within(() => {
-           cy.getByTestId(`invalid-feedback`).should("be.visible")
-           cy.getByTestId(`user-choicerow-radio-52`).getByTestId(`user-choicerow-radio-checked-52`).click({ force: true })
+        cy.getByTestId(`invalid-feedback`).should("be.visible")
+        cy.getByTestId(`user-choicerow-radio-52`)
+          .getByTestId(`user-choicerow-radio-checked-52`)
+          .click({ force: true })
 
-           cy.contains(/submit/i).click()
-           cy.contains(/submitting/i).should("be.visible")
+        cy.contains(/submit/i).click()
+        cy.contains(/submitting/i).should("be.visible")
 
-           cy.wait("@submit")
+        cy.wait("@submit")
 
-           cy.contains("Thanks!").should("be.visible")
-           cy.contains(/done/i).click()
-        })
+        cy.contains("Thanks!").should("be.visible")
+        cy.contains(/done/i).click()
+      })
       cy.lightbox().should("not.exist")
     })
   })
@@ -53,9 +64,18 @@ describe("Question", () => {
       cy.contains(/question text/i).click()
       cy.focused().type(question)
       cy.getByTestId("question-answer-multiple-choice").click({ force: true })
-      cy.getByTestId(`choicerow-checkbox-1`).getByTestId(`choicerow-checkbox-input-1`).click().type("1")
-      cy.getByTestId(`choicerow-checkbox-2`).getByTestId(`choicerow-checkbox-input-2`).click().type("2")
-      cy.getByTestId(`choicerow-checkbox-3`).getByTestId(`choicerow-checkbox-input-3`).click().type("3")
+      cy.getByTestId(`choicerow-checkbox-1`)
+        .getByTestId(`choicerow-checkbox-input-1`)
+        .click()
+        .type("1")
+      cy.getByTestId(`choicerow-checkbox-2`)
+        .getByTestId(`choicerow-checkbox-input-2`)
+        .click()
+        .type("2")
+      cy.getByTestId(`choicerow-checkbox-3`)
+        .getByTestId(`choicerow-checkbox-input-3`)
+        .click()
+        .type("3")
 
       cy.submitModal()
       cy.openLightbox(node.id)
@@ -63,17 +83,19 @@ describe("Question", () => {
       cy.route("POST", "**/quiz*").as("submit")
 
       cy.lightbox().within(() => {
-           cy.getByTestId(`invalid-feedback`).should("be.visible")
-           cy.getByTestId(`user-choicerow-checkbox-2`).getByTestId(`user-choicerow-checkbox-checked-2`).click({ force: true })
+        cy.getByTestId(`invalid-feedback`).should("be.visible")
+        cy.getByTestId(`user-choicerow-checkbox-2`)
+          .getByTestId(`user-choicerow-checkbox-checked-2`)
+          .click({ force: true })
 
-           cy.contains(/submit/i).click()
-           cy.contains(/submitting/i).should("be.visible")
+        cy.contains(/submit/i).click()
+        cy.contains(/submitting/i).should("be.visible")
 
-           cy.wait("@submit")
+        cy.wait("@submit")
 
-           cy.contains("Thanks!").should("be.visible")
-           cy.contains(/done/i).click()
-        })
+        cy.contains("Thanks!").should("be.visible")
+        cy.contains(/done/i).click()
+      })
       cy.lightbox().should("not.exist")
     })
   })
@@ -90,15 +112,26 @@ describe("Question", () => {
       cy.contains(/question text/i).click()
       cy.focused().type(question)
       cy.getByTestId("question-answer-multiple-choice").click({ force: true })
-      cy.getByTestId(`choicerow-checkbox-1`).getByTestId(`choicerow-checkbox-input-1`).click().type("1")
-      cy.getByTestId(`choicerow-checkbox-2`).getByTestId(`choicerow-checkbox-input-2`).click().type("2")
-      cy.getByTestId(`choicerow-checkbox-3`).getByTestId(`choicerow-checkbox-input-3`).click().type("3")
+      cy.getByTestId(`choicerow-checkbox-1`)
+        .getByTestId(`choicerow-checkbox-input-1`)
+        .click()
+        .type("1")
+      cy.getByTestId(`choicerow-checkbox-2`)
+        .getByTestId(`choicerow-checkbox-input-2`)
+        .click()
+        .type("2")
+      cy.getByTestId(`choicerow-checkbox-3`)
+        .getByTestId(`choicerow-checkbox-input-3`)
+        .click()
+        .type("3")
 
-      cy.getByTestId('multiplechoice-thumbnail').click({ force: true })
+      cy.getByTestId("multiplechoice-thumbnail").click({ force: true })
 
       cy.server()
       cy.route("POST", "**/async-upload.php").as("upload")
-      cy.getByTestId("choicerow-checkbox-thumbnail-1").getByTestId("import-file-input").attachFile("reddit.png")
+      cy.getByTestId("choicerow-checkbox-thumbnail-1")
+        .getByTestId("import-file-input")
+        .attachFile("reddit.png")
 
       cy.wait("@upload")
         .its("response.body.data.url")
@@ -107,32 +140,39 @@ describe("Question", () => {
           cy.get(".alert-success").should("exist")
           cy.server()
           cy.route("POST", "**/async-upload.php").as("upload")
-          cy.getByTestId("choicerow-checkbox-thumbnail-2").getByTestId("import-file-input").attachFile("reddit.png")
-          
-            cy.wait("@upload")
-               .its("response.body.data.url")
-               .then(() => {
+          cy.getByTestId("choicerow-checkbox-thumbnail-2")
+            .getByTestId("import-file-input")
+            .attachFile("reddit.png")
+
+          cy.wait("@upload")
+            .its("response.body.data.url")
+            .then(() => {
+              cy.get("No image").should("not.exist")
+              cy.get(".alert-success").should("exist")
+              cy.server()
+              cy.route("POST", "**/async-upload.php").as("upload")
+              cy.getByTestId("choicerow-checkbox-thumbnail-3")
+                .getByTestId("import-file-input")
+                .attachFile("reddit.png")
+
+              cy.wait("@upload")
+                .its("response.body.data.url")
+                .then(() => {
                   cy.get("No image").should("not.exist")
                   cy.get(".alert-success").should("exist")
-                  cy.server()
-                  cy.route("POST", "**/async-upload.php").as("upload")
-                  cy.getByTestId("choicerow-checkbox-thumbnail-3").getByTestId("import-file-input").attachFile("reddit.png")
-    
-                  cy.wait("@upload")
-                  .its("response.body.data.url")
-                  .then(() => {  
-                    
-                    cy.get("No image").should("not.exist")
-                    cy.get(".alert-success").should("exist")
-                    cy.submitModal()
-                    cy.openLightbox(node.id)
+                  cy.submitModal()
+                  cy.openLightbox(node.id)
 
-                    cy.route("POST", "**/quiz*").as("submit")
+                  cy.route("POST", "**/quiz*").as("submit")
 
-                    cy.lightbox().within(() => {
-                    cy.getByTestId('user-choicerow-checkbox-thumbnail-1').should("be.visible")
+                  cy.lightbox().within(() => {
+                    cy.getByTestId("user-choicerow-checkbox-thumbnail-1").should(
+                      "be.visible"
+                    )
                     cy.getByTestId(`invalid-feedback`).should("exist")
-                    cy.getByTestId(`user-choicerow-checkbox-2`).getByTestId(`user-choicerow-checkbox-checked-2`).click({ force: true })
+                    cy.getByTestId(`user-choicerow-checkbox-2`)
+                      .getByTestId(`user-choicerow-checkbox-checked-2`)
+                      .click({ force: true })
 
                     cy.contains(/submit/i).click()
                     cy.contains(/submitting/i).should("be.visible")
@@ -141,16 +181,15 @@ describe("Question", () => {
 
                     cy.contains("Thanks!").should("be.visible")
                     cy.contains(/done/i).click()
-                    })
-                    cy.lightbox().should("not.exist")
-
-                   })          
-               })
-          })
+                  })
+                  cy.lightbox().should("not.exist")
+                })
+            })
         })
     })
-  
-    it("should be able to complete with a radio answer with images", () => {
+  })
+
+  it("should be able to complete with a radio answer with images", () => {
     cy.setup("@oneNode")
 
     cy.getSelectedNode().then(node => {
@@ -163,15 +202,26 @@ describe("Question", () => {
       cy.focused().type(question)
       cy.getByTestId("question-answer-multiple-choice").click({ force: true })
       cy.getByTestId("question-answer-multipleChoice-single").click({ force: true })
-      cy.getByTestId(`choicerow-radio-50`).getByTestId(`choicerow-radio-input-50`).click().type("4")
-      cy.getByTestId(`choicerow-radio-51`).getByTestId(`choicerow-radio-input-51`).click().type("8")
-      cy.getByTestId(`choicerow-radio-52`).getByTestId(`choicerow-radio-input-52`).click().type("10")
+      cy.getByTestId(`choicerow-radio-50`)
+        .getByTestId(`choicerow-radio-input-50`)
+        .click()
+        .type("4")
+      cy.getByTestId(`choicerow-radio-51`)
+        .getByTestId(`choicerow-radio-input-51`)
+        .click()
+        .type("8")
+      cy.getByTestId(`choicerow-radio-52`)
+        .getByTestId(`choicerow-radio-input-52`)
+        .click()
+        .type("10")
 
-      cy.getByTestId('multiplechoice-thumbnail').click({ force: true })
+      cy.getByTestId("multiplechoice-thumbnail").click({ force: true })
 
       cy.server()
       cy.route("POST", "**/async-upload.php").as("upload")
-      cy.getByTestId("choicerow-radio-thumbnail-50").getByTestId("import-file-input").attachFile("reddit.png")
+      cy.getByTestId("choicerow-radio-thumbnail-50")
+        .getByTestId("import-file-input")
+        .attachFile("reddit.png")
 
       cy.wait("@upload")
         .its("response.body.data.url")
@@ -180,32 +230,39 @@ describe("Question", () => {
           cy.get(".alert-success").should("exist")
           cy.server()
           cy.route("POST", "**/async-upload.php").as("upload")
-          cy.getByTestId("choicerow-radio-thumbnail-51").getByTestId("import-file-input").attachFile("reddit.png")
-          
-            cy.wait("@upload")
-               .its("response.body.data.url")
-               .then(() => {
+          cy.getByTestId("choicerow-radio-thumbnail-51")
+            .getByTestId("import-file-input")
+            .attachFile("reddit.png")
+
+          cy.wait("@upload")
+            .its("response.body.data.url")
+            .then(() => {
+              cy.get("No image").should("not.exist")
+              cy.get(".alert-success").should("exist")
+              cy.server()
+              cy.route("POST", "**/async-upload.php").as("upload")
+              cy.getByTestId("choicerow-radio-thumbnail-52")
+                .getByTestId("import-file-input")
+                .attachFile("reddit.png")
+
+              cy.wait("@upload")
+                .its("response.body.data.url")
+                .then(() => {
                   cy.get("No image").should("not.exist")
                   cy.get(".alert-success").should("exist")
-                  cy.server()
-                  cy.route("POST", "**/async-upload.php").as("upload")
-                  cy.getByTestId("choicerow-radio-thumbnail-52").getByTestId("import-file-input").attachFile("reddit.png")
-    
-                  cy.wait("@upload")
-                  .its("response.body.data.url")
-                  .then(() => {  
-                    
-                    cy.get("No image").should("not.exist")
-                    cy.get(".alert-success").should("exist")
-                    cy.submitModal()
-                    cy.openLightbox(node.id)
+                  cy.submitModal()
+                  cy.openLightbox(node.id)
 
-                    cy.route("POST", "**/quiz*").as("submit")
+                  cy.route("POST", "**/quiz*").as("submit")
 
-                    cy.lightbox().within(() => {
-                    cy.getByTestId('user-choicerow-radio-thumbnail-50').should("be.visible")
+                  cy.lightbox().within(() => {
+                    cy.getByTestId("user-choicerow-radio-thumbnail-50").should(
+                      "be.visible"
+                    )
                     cy.getByTestId(`invalid-feedback`).should("exist")
-                    cy.getByTestId(`user-choicerow-radio-52`).getByTestId(`user-choicerow-radio-checked-52`).click({ force: true })
+                    cy.getByTestId(`user-choicerow-radio-52`)
+                      .getByTestId(`user-choicerow-radio-checked-52`)
+                      .click({ force: true })
 
                     cy.contains(/submit/i).click()
                     cy.contains(/submitting/i).should("be.visible")
@@ -214,14 +271,13 @@ describe("Question", () => {
 
                     cy.contains("Thanks!").should("be.visible")
                     cy.contains(/done/i).click()
-                    })
-                    cy.lightbox().should("not.exist")
-
-                   })          
-               })
-          })
+                  })
+                  cy.lightbox().should("not.exist")
+                })
+            })
         })
     })
+  })
 
   it("should be able to complete with a text-based answer", () => {
     cy.setup("@oneNode")
@@ -311,5 +367,4 @@ describe("Question", () => {
       cy.lightbox().should("not.exist")
     })
   })
-
 })
