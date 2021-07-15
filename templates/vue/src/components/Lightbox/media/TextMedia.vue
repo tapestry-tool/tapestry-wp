9dbe1727ea7790bf296a1aaab08fe831fed38bac
wynnset <aidin@wynnset.com>
2021-07-14 17:53:13 -0700

    reduce unnecessary code

--- templates/vue/src/components/Lightbox/media/TextMedia.vue
+++ templates/vue/src/components/Lightbox/media/TextMedia.vue
@@ -1,11 +1,5 @@
 <template>
-  <article
-    :class="{
-      article: true,
-      multiContentArticle: context === 'multi-content' || context === 'page',
-      'page-style': context == 'page',
-    }"
-  >
+  <article :class="'article context-' + context">
     <h1 v-if="showTitle">{{ node.title }}</h1>
     <div v-html="content"></div>
   </article>
@@ -46,7 +40,6 @@ export default {
 <style lang="scss" scoped>
 .article {
   padding: 1em;
-  background: rgb(238, 238, 238);
   text-align: left;
   min-height: 100%;
 
@@ -64,39 +57,6 @@ export default {
     font-family: "Source Sans Pro", sans-serif;
     font-size: 16px;
     white-space: pre-wrap;
-    margin-bottom: 0.9em;
-  }
-}
-
-.multiContentArticle {
-  padding: 1em;
-  background: rgb(38, 38, 38);
-  text-align: left;
-  min-height: 100%;
-
-  h1 {
-    font-size: 1.75rem;
-    font-weight: 500;
-    margin-bottom: 1em;
-
-    :before {
-      display: none;
-    }
-  }
-
-  div {
-    font-family: "Source Sans Pro", sans-serif;
-    font-size: 16px;
-    white-space: pre-wrap;
-    margin-bottom: 0.9em;
-  }
-}
-
-.page-style {
-  padding: 0 !important;
-
-  > h1 {
-    margin-top: 0 !important;
     margin-bottom: 0.9em;
   }
 }
