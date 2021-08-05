919e617aa4a1fea9c4a8b4f64bd4c1201cb22883
EldadZZipori <eldad.z.zipori@gmail.com>
2021-08-05 12:35:23 -0700

    fix cypress

--- templates/vue/cypress/integration/user-settings.spec.js
+++ templates/vue/cypress/integration/user-settings.spec.js
@@ -5,18 +5,14 @@ describe("User Settings", () => {
   })
 
   it(`should be able to open user settings modal and change theme settings`, () => {
-    cy.store()
-      .its("state.theme")
-      .then(() => {
-        cy.getByTestId("user-settings-button").click({ force: true })
+    cy.getByTestId("user-settings-button").click({ force: true })
 
-        cy.contains(/light mode/i).click()
-        cy.contains(/save theme/i).click()
+    cy.contains(/light mode/i).click()
+    cy.contains(/save theme/i).click()
 
-        cy.getByTestId("user-settings-button").click({ force: true })
+    cy.getByTestId("user-settings-button").click({ force: true })
 
-        cy.contains(/dark mode/i).click()
-        cy.contains(/save theme/i).click()
-      })
+    cy.contains(/dark mode/i).click()
+    cy.contains(/save theme/i).click()
   })
 })
