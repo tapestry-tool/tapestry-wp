6c5a05e9ec7ad92aa0fb0c51b511bf667bba3394
EldadZZipori <eldad.z.zipori@gmail.com>
2021-08-13 13:12:18 -0700

    additonal remote cypress test

--- endpoints.php
+++ endpoints.php
@@ -15,8 +15,9 @@ require_once __DIR__.'/utilities/class.tapestry-user.php';
 
 
 define('LOAD_KALTURA', !empty(KALTURA_ADMIN_SECRET) && !empty(KALTURA_PARTNER_ID) && !empty(KALTURA_SERVICE_URL) );
-//require_once __DIR__.'/services/class.kaltura-api.php';
-
+if (LOAD_KALTURA) {
+    require_once __DIR__.'/services/class.kaltura-api.php';
+}
 
 $REST_API_NAMESPACE = 'tapestry-tool/v1';
 
