c9985bcb7654d3e8e73872688a19932df306536b
EldadZZipori <eldad.z.zipori@gmail.com>
2021-08-19 12:14:40 -0700

    Update AccordionRows.vue

--- templates/vue/src/components/Lightbox/media/MultiContentMedia/AccordionRows.vue
+++ templates/vue/src/components/Lightbox/media/MultiContentMedia/AccordionRows.vue
@@ -178,8 +178,8 @@ export default {
     rowBackground() {
       if (this.isMultiContentContext) {
         let rgb = 187
-        // let colorOffset = this.level * 10
-        // rgb = colorOffset > rgb ? 0 : rgb - colorOffset
+        let colorOffset = this.level * 10
+        rgb = colorOffset > rgb ? 0 : rgb - colorOffset
         return {
           background: `rgb(${rgb}, ${rgb}, ${rgb})`,
         }
