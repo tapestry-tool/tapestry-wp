dc95a8502d216d13762bcf691835491bcc6a25b3
EldadZZipori <eldad.z.zipori@gmail.com>
2021-08-19 12:04:51 -0700

    keep side menu test the same color

--- templates/vue/src/components/Toolbar/TapestryDepthSlider.vue
+++ templates/vue/src/components/Toolbar/TapestryDepthSlider.vue
@@ -308,6 +308,7 @@ export default {
   width: 100%;
   top: calc(100% - 1px);
   right: 0;
+  color: black;
   box-shadow: 0 7px 7px 0 #ddd;
   border-top-right-radius: 0;
   border-top-left-radius: 0;
