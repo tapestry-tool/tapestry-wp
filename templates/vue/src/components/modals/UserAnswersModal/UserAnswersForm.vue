4f2bbe22da0041d688ecf9f27235bf39de7e9117
Enid <edotadot2@gmail.com>
2021-08-30 12:24:04 -0700

    add: select question form

--- templates/vue/src/components/modals/UserAnswersModal/UserAnswersForm.vue
+++ templates/vue/src/components/modals/UserAnswersModal/UserAnswersForm.vue
@@ -1,10 +1,42 @@
 <template>
   <div class="px-3 mt-n2">
     <b-overlay variant="white">
-      <h6 class="mb-3">Customize your avatar</h6>
       <b-row>
-        <b-col cols="8" class="customizer pt-1">
-          Listing answers table
+        <b-col cols="9">
+          <div>
+            <b-form-group data-qa="select-activity" label="Activity">
+              <combobox
+                v-model="activityId"
+                :options="activityNodes"
+                data-qa="choose-activity"
+                item-text="title"
+                item-value="id"
+                empty-message="Please select an activity first"
+              >
+                <template v-slot="slotProps">
+                  <p>
+                    {{ slotProps.option.title }}
+                  </p>
+                </template>
+              </combobox>
+            </b-form-group>
+            <b-form-group data-qa="select-question" label="Question">
+              <combobox
+                v-model="questionId"
+                :options="availableQuestions"
+                data-qa="choose-question"
+                item-text="text"
+                item-value="id"
+                empty-message="Please select an activity first."
+              >
+                <template v-slot="slotProps">
+                  <p>
+                    {{ slotProps.option.text }}
+                  </p>
+                </template>
+              </combobox>
+            </b-form-group>
+          </div>
         </b-col>
       </b-row>
     </b-overlay>
@@ -12,13 +44,40 @@
 </template>
 
 <script>
+import { mapState, mapGetters } from "vuex"
+import Combobox from "@/components/modals/common/Combobox"
 export default {
   name: "user-answers-form",
+  components: {
+    Combobox,
+  },
   props: {},
   data() {
-    return {}
+    return {
+      activityId: null,
+      questionId: null,
+    }
   },
-  computed: {},
+  computed: {
+    ...mapState(["nodes"]),
+    ...mapGetters(["getNode"]),
+    activityNode() {
+      return this.getNode(this.activityId)
+    },
+    question() {
+      return this.availableQuestions.find(
+        question => question.id === this.questionId
+      )
+    },
+    activityNodes() {
+      return Object.values(this.nodes).filter(node => node.mediaType == "activity")
+    },
+    availableQuestions() {
+      return Object.values(this.activityNodes)
+        .filter(node => node.id == this.activityId)
+        .flatMap(node => node.typeData.activity.questions)
+    },
+  },
   methods: {},
 }
 </script>
