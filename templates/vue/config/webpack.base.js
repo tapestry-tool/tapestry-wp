66d31c3311ac25d7f32ef998d1df0bdf287f23ae
Legendword <zwh@legendword.com>
2022-05-25 12:32:02 -0700

    upgrade to webpack v5
    - following https://webpack.js.org/migrate/5/

--- templates/vue/config/webpack.base.js
+++ templates/vue/config/webpack.base.js
@@ -32,7 +32,7 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: "file-loader",
         options: {
-          name: "[name].[ext]?[hash]",
+          name: "[name].[ext]?[contentHash]",
         },
       },
     ],
