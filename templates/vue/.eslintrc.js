3eb5fa4463b6d2a2ea8bc37db145c4fade809a4f
Legendword <zwh@legendword.com>
2022-05-26 17:23:18 -0700

    turning off some rules
    fix a small rule error in MultipleChoiceQuestion

--- templates/vue/.eslintrc.js
+++ templates/vue/.eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
     "plugin:prettier/recommended",
   ],
   rules: {
+    "vue/v-slot-style": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/no-mutating-props": "off",
     "vue/attribute-hyphenation": "off",
     "vue/component-name-in-template-casing": ["error", "kebab-case"],
     "vue/component-definition-name-casing": ["error", "kebab-case"],
