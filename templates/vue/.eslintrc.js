7f3b7fa2e667aa59274778f65e1b2ece9d0c8d30
Legendword <zwh@legendword.com>
2022-05-26 17:30:13 -0700

    Re-enable vue/no-mutating-props rule

--- templates/vue/.eslintrc.js
+++ templates/vue/.eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
   rules: {
     "vue/v-slot-style": "off",
     "vue/multi-word-component-names": "off",
-    "vue/no-mutating-props": "off",
     "vue/attribute-hyphenation": "off",
     "vue/component-name-in-template-casing": ["error", "kebab-case"],
     "vue/component-definition-name-casing": ["error", "kebab-case"],
