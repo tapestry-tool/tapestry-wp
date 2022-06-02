b13484a7ead8847be0538f0884d99c7330146de3
Floria Gu <gfzting@gmail.com>
2022-06-02 12:57:11 -0700

    Make 'clean h5p nodes' a button

--- settings.php
+++ settings.php
@@ -14,12 +14,11 @@ function tapestry_settings_init()
 {
     add_settings_section('tapestry_db_settings', 'Database Settings', 'tapestry_db_section_cb', 'tapestry_settings_page');
 
-    add_settings_field('tapestry_clean_h5p_nodes', 'Clean h5p Nodes', 'tapestry_clean_h5p_nodes_cb', 'tapestry_settings_page', 'tapestry_db_settings');
-
     add_settings_section('tapestry_kaltura_upload_dashboard', 'Kaltura Video Upload', 'tapestry_kaltura_upload_dashboard_cb', 'tapestry_settings_page');
 }
 
-function load_tapestry_settings_page_scripts($hook) {
+function load_tapestry_settings_page_scripts($hook)
+{
     // TO DO: find a better way to test the current settings page
     if (str_ends_with($hook, 'tapestry_settings_page')) {
         $prefix = get_rest_url(null, 'tapestry-tool/v1');
@@ -40,22 +39,18 @@ function tapestry_settings_page_cb()
 {
     ?>
     <h2>Tapestry Plugin Settings</h2>
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <?php
-            do_settings_sections('tapestry_settings_page'); ?>
-        <input type="submit" value="Run" name="tapestry_db"/>
-    </form>
     <?php
+        do_settings_sections('tapestry_settings_page');
 }
 
 function tapestry_db_section_cb()
 {
-    echo 'Perform database modifications';
-}
-
-function tapestry_clean_h5p_nodes_cb()
-{
-    echo "<input type='checkbox' id='clean_h5p_nodes' name='clean_h5p_nodes'  value='1'/>";
+    echo '<p>Perform database modifications</p>'; ?>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <?php
+            submit_button('Clean h5p Nodes', 'primary', 'clean_h5p_nodes', false); ?>
+    </form>
+    <?php
 }
 
 function tapestry_kaltura_upload_dashboard_cb()
@@ -78,7 +73,7 @@ function tapestry_kaltura_upload_dashboard_cb()
         </thead>
     </table>
     ';
-    
+
     echo '<p>';
     submit_button('Refresh Now', 'secondary', 'refresh_kaltura_upload_progress', false, array('onclick' => 'refreshKalturaUploadProgress()'));
     echo '</p>';
@@ -86,35 +81,33 @@ function tapestry_kaltura_upload_dashboard_cb()
 
 function run_db_commands()
 {
-    if (isset($_POST['tapestry_db'])) {
-        if (isset($_POST['clean_h5p_nodes'])) {
-            $query_args = array(
-                'post_type' => 'tapestry_node',
-                'meta_query' => array(
-                    array(
-                       'key'     => 'tapestry_node_data',
-                       'value'   => 'h5p',
-                       'compare' => 'LIKE',
-                    ),
-                    array(
-                        'key'     => 'tapestry_node_data',
-                        'value'   => 'video',
-                        'compare' => 'LIKE',
-                    ),
+    if (isset($_POST['clean_h5p_nodes'])) {
+        $query_args = array(
+            'post_type' => 'tapestry_node',
+            'meta_query' => array(
+                array(
+                   'key'     => 'tapestry_node_data',
+                   'value'   => 'h5p',
+                   'compare' => 'LIKE',
                 ),
-            );
-            $h5p_query = new WP_Query($query_args);
-            while ($h5p_query->have_posts()) {
-                $h5p_query->the_post();
-                $post_id = get_the_ID();
-                $node_meta_data = get_post_meta($post_id, 'tapestry_node_data', true);
-                if (isset($node_meta_data->mediaFormat) && $node_meta_data->mediaFormat == 'h5p' && isset($node_meta_data->mediaType) && $node_meta_data->mediaType == 'video') {
-                    $node_meta_data->mediaType = 'h5p';
-                    update_post_meta($post_id, 'tapestry_node_data', $node_meta_data);
-                }
+                array(
+                    'key'     => 'tapestry_node_data',
+                    'value'   => 'video',
+                    'compare' => 'LIKE',
+                ),
+            ),
+        );
+        $h5p_query = new WP_Query($query_args);
+        while ($h5p_query->have_posts()) {
+            $h5p_query->the_post();
+            $post_id = get_the_ID();
+            $node_meta_data = get_post_meta($post_id, 'tapestry_node_data', true);
+            if (isset($node_meta_data->mediaFormat) && $node_meta_data->mediaFormat == 'h5p' && isset($node_meta_data->mediaType) && $node_meta_data->mediaType == 'video') {
+                $node_meta_data->mediaType = 'h5p';
+                update_post_meta($post_id, 'tapestry_node_data', $node_meta_data);
             }
-            add_action('admin_notices', 'tapestry_h5p_conf_notice');
         }
+        add_action('admin_notices', 'tapestry_h5p_conf_notice');
     }
 }
 
