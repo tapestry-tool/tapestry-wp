f8fb18f80b27b6c13bb30c774b2144844d75fd6b
Legendword <zwh@legendword.com>
2022-06-30 14:13:11 -0700

    Add node hidden test

--- templates/vue/cypress/integration/node-locked.spec.js
+++ templates/vue/cypress/integration/node-locked.spec.js
@@ -51,4 +51,53 @@ describe("Locked Nodes", () => {
         )
       })
   })
+
+  it("should be able to hide a node when locked", () => {
+    cy.store()
+      .its("state.nodes")
+      .then(nodes => {
+        const [root, child] = Object.values(nodes)
+
+        const condition = {
+          type: conditionTypes.NODE_COMPLETED,
+          nodeId: root.id,
+        }
+
+        cy.openModal("edit", child.id)
+        cy.contains(/access/i).click()
+        cy.contains(/prevent access/i).click()
+        cy.contains(/hide this node when locked/i).click()
+
+        cy.getByTestId("add-condition").click()
+        cy.getByTestId("condition-type").select(condition.type)
+        cy.getByTestId("condition-id").select(String(condition.nodeId))
+
+        cy.submitModal()
+
+        cy.getByTestId(`open-node-${child.id}`).should(
+          "not.have.attr",
+          "aria-disabled"
+        )
+
+        cy.getByTestId(`node-${child.id}`)
+          .invoke("css", "opacity")
+          .should("not.equal", "1")
+
+        cy.login("subscriber").visitTapestry()
+        cy.getByTestId(`node-${child.id}`).should("not.be.visible")
+
+        cy.intercept("POST", "**/progress").as("complete")
+
+        cy.openLightbox(root.id).should("exist")
+        cy.closeLightbox()
+
+        cy.wait("@complete")
+
+        cy.getByTestId(`node-${child.id}`).should("be.visible")
+        cy.getByTestId(`open-node-${child.id}`).should(
+          "not.have.attr",
+          "aria-disabled"
+        )
+      })
+  })
 })
