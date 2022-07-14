d3eab91afa0faad6d1f91a5cac09cc31c66bdaba
zifgu <gfzting@gmail.com>
2022-07-13 19:19:30 -0700

    Try using --disable-dev-shm-usage
    - Removes shm_size option
    - Sets browser back to chrome

--- templates/vue/cypress/plugins/index.js
+++ templates/vue/cypress/plugins/index.js
@@ -20,5 +20,13 @@ module.exports = (on, config) => {
       return null
     },
   })
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    if (browser.family === "chrome") {
+      console.log("Adding --disable-dev-shm-usage...")
+      launchOptions.args.push("--disable-dev-shm-usage")
+    }
+
+    return launchOptions
+  })
   return config
 }
