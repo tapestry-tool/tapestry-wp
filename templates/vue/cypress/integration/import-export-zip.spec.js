391d06064d62aef2fda3c206fa4ebd93538aac9b
zifgu <gfzting@gmail.com>
2022-07-14 14:20:23 -0700

    Comment out some lines to test if the test fails

--- templates/vue/cypress/integration/import-export-zip.spec.js
+++ templates/vue/cypress/integration/import-export-zip.spec.js
@@ -27,10 +27,10 @@ describe("Import Export Zip Files", () => {
       .should("have.length", 3)
     cy.task("log", "Line 28")
 
-    cy.contains("button", /confirm/i).click()
-    cy.task("log", "Line 31")
-    cy.wait("@load")
-    cy.task("log", "Line 33")
+    // cy.contains("button", /confirm/i).click()
+    // cy.task("log", "Line 31")
+    // cy.wait("@load")
+    // cy.task("log", "Line 33")
 
     cy.getByTestId("tapestry-loading").should("not.exist")
     cy.task("log", "Line 36")
@@ -72,10 +72,10 @@ describe("Import Export Zip Files", () => {
       .should("have.length", 5)
     cy.task("log", "Line 73")
 
-    cy.contains("button", /confirm/i).click()
-    cy.task("log", "Line 76")
-    cy.wait("@load")
-    cy.task("log", "Line 78")
+    // cy.contains("button", /confirm/i).click()
+    // cy.task("log", "Line 76")
+    // cy.wait("@load")
+    // cy.task("log", "Line 78")
 
     cy.getByTestId("tapestry-loading").should("not.exist")
     cy.task("log", "Line 81")
