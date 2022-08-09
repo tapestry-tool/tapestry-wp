aa91c05c3c4d50511565c0e42095bca3e813ce58
zifgu <gfzting@gmail.com>
2022-08-08 18:04:21 -0700

    Mock getKalturaAvailableLanguages call to fix failing test

--- templates/vue/src/components/modals/NodeModal.spec.js
+++ templates/vue/src/components/modals/NodeModal.spec.js
@@ -7,6 +7,13 @@ import { names } from "@/config/routes"
 
 const node = oneNodeTapestry.nodes[0]
 
+jest.mock("@/services/TapestryAPI", () => {
+  return {
+    ...jest.requireActual("@/services/TapestryAPI"),
+    getKalturaAvailableLanguages: jest.fn(),
+  }
+})
+
 describe("node modal: content - video", () => {
   let screen
 
