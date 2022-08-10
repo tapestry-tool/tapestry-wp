caf4fe18fc0e3cbc9d0d67c265debc483865a232
zifgu <gfzting@gmail.com>
2022-08-09 21:30:08 -0700

    Prevent subscribers from being able to publish draft-turned-accepted nodes

--- classes/class.tapestry.php
+++ classes/class.tapestry.php
@@ -169,7 +169,7 @@ class Tapestry implements ITapestry
 
         // Checks if user is logged in to prevent logged out user-0 from getting permissions
         // Only add user permissions if it is not a review node
-        if (is_user_logged_in() && 0 === count($node->reviewComments)) {
+        if (is_user_logged_in() && $node->status !== "draft") {
             $userId = wp_get_current_user()->ID;
             $node->permissions->{'user-'.$userId} = ['read', 'add', 'edit'];
         }
