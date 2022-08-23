ccb01f0f8cedd84aa21a2d397cd2b1e8724b283f
Legendword <zwh@legendword.com>
2022-08-22 17:05:12 -0700

    Improve panning animation for click-to-zoom
    by pre-determining the clamped offset and then animate based on that adjusted target offset, rather than let clamping happen mid-animation

--- templates/vue/src/utils/interpolate.js
+++ templates/vue/src/utils/interpolate.js
@@ -1,25 +1,30 @@
 import * as d3 from "d3"
 
-const easingFunction = d3.easeCubicOut
+const easingFunctions = {
+  easeOut: d3.easeCubicOut,
+  linear: d3.easeLinear,
+}
 
 /**
- * Interpolates number between [from] and [to], in [duration] seconds.
+ * Interpolates between [from] and [to], in [duration] seconds.
  * Calls onChange while value is being changed with the intermediate value as the only argument
  * Calls onComplete when the interpolation finishes
- * @param {Number} from
- * @param {Number} to
+ * @param {any} from
+ * @param {any} to
  * @param {Number} duration
  * @param {Function} onChange
  * @param {Function} onComplete
+ * @param {String} easing
  */
 export function interpolate(
   from,
   to,
   duration,
   onChange = () => {},
-  onComplete = () => {}
+  onComplete = () => {},
+  easing = "linear"
 ) {
-  const interpolator = d3.interpolateNumber(from, to)
+  const interpolator = d3.interpolate(from, to)
   const timer = d3.timer(elapsed => {
     if (elapsed > duration) {
       onChange(to)
@@ -27,7 +32,7 @@ export function interpolate(
       onComplete()
     } else {
       const t = elapsed / duration
-      onChange(interpolator(easingFunction(t)))
+      onChange(interpolator(easingFunctions[easing](t)))
     }
   })
 }
@@ -41,13 +46,15 @@ export function interpolate(
  * @param {Number} duration
  * @param {Function} onChange
  * @param {Function} onComplete
+ * @param {String} easing
  */
 export function interpolateDelta(
   from,
   to,
   duration,
   onChange = () => {},
-  onComplete = () => {}
+  onComplete = () => {},
+  easing = "linear"
 ) {
   const interpolator = d3.interpolateNumber(from, to)
   let lastValue = from
@@ -58,7 +65,7 @@ export function interpolateDelta(
       onComplete()
     } else {
       const t = elapsed / duration
-      const value = interpolator(easingFunction(t))
+      const value = interpolator(easingFunctions[easing](t))
       onChange(value - lastValue)
       lastValue = value
     }
