5b0889cd9d096221efc8c48f36f34a57cbc740cc
zifgu <gfzting@gmail.com>
2022-08-23 11:06:25 -0700

    Make upload error dismissible; clear upload error when "Clean Uploaded Videos" run

--- templates/vue/src/services/TapestryAPI.js
+++ templates/vue/src/services/TapestryAPI.js
@@ -346,6 +346,11 @@ class TapestryApi {
     const url = `/kaltura/stop_upload`
     await this.client.post(url)
   }
+
+  async clearKalturaUploadError() {
+    const url = `/kaltura/upload_status/clear_error`
+    await this.client.post(url)
+  }
 }
 
 export default new TapestryApi(postId)
