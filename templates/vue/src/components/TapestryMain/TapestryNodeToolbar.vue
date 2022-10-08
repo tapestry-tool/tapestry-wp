1996e993048b69a29ee93e53868c2c62d9a2aa7e
Legendword <zwh@legendword.com>
2022-10-07 19:52:50 -0700

    Hide title button on node-specific toolbar

--- templates/vue/src/components/TapestryMain/TapestryNodeToolbar.vue
+++ templates/vue/src/components/TapestryMain/TapestryNodeToolbar.vue
@@ -1,20 +1,28 @@
 <template>
   <tapestry-context-toolbar :target="nodeElementId">
     <tapestry-toolbar-button
-      :id="`add-node-tool-${node.id}`"
-      horizontal
-      :tool="tools.ADD_NODE"
-      tooltip="Add Node"
-    >
-      <i class="fas fa-plus-circle fa-lg"></i>
-    </tapestry-toolbar-button>
-    <div class="tapestry-toolbar-separator"></div>
-    <tapestry-toolbar-button
       :id="`delete-node-button-${node.id}`"
       horizontal
       tooltip="Delete Node"
     >
       <i class="fas fa-trash-alt fa-lg"></i>
+    </tapestry-toolbar-button>
+    <div class="tapestry-toolbar-separator"></div>
+    <tapestry-toolbar-button
+      :id="`hide-title-button-${node.id}`"
+      horizontal
+      :tooltip="node.hideTitle ? 'Show Title' : 'Hide Title'"
+      @click="toggleHideTitle"
+    >
+      <div
+        class="hide-title-text"
+        :class="{
+          'hide-title': node.hideTitle,
+        }"
+      >
+        Title
+        <div v-if="node.hideTitle" class="slash"></div>
+      </div>
     </tapestry-toolbar-button>
   </tapestry-context-toolbar>
 </template>
@@ -24,6 +32,7 @@ import TapestryContextToolbar from "./TapestryContextToolbar"
 import TapestryToolbarButton from "./common/TapestryToolbarButton"
 import Helpers from "@/utils/Helpers"
 import { tools } from "@/utils/constants"
+import { mapActions } from "vuex"
 
 export default {
   components: {
@@ -46,5 +55,37 @@ export default {
       return Helpers.getNodeElementId(this.node.id)
     },
   },
+  methods: {
+    ...mapActions(["updateNode"]),
+    toggleHideTitle() {
+      this.updateNode({
+        id: this.node.id,
+        newNode: {
+          hideTitle: !this.node.hideTitle,
+        },
+      })
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.hide-title-text {
+  position: relative;
+
+  &.hide-title {
+    color: #b2b2b2;
+  }
+}
+
+.slash {
+  width: 2px;
+  height: 3rem;
+  background: #000;
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: rotate(-70deg) translate(-6px, -4px);
+  transform-origin: 0 0;
+}
+</style>
