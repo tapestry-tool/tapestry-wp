4ccf0af9e7e7bd171cc7e034345698eaabdfeb54
wynnset <aidin@wynnset.com>
2022-11-24 15:42:27 -0800

    Fix issue with hard-coded image path

--- templates/footer-iframe.php
+++ templates/footer-iframe.php
@@ -4,11 +4,11 @@
 <div style="width: 100%;bottom: 2px;position: fixed;left: 2px;display: flex;align-items: center;justify-content: space-between;">
     <div style="display: flex;align-items: center;justify-content: center;">
         <a href="https://tapestry-tool.com" target="_blank" style="">
-            <img width="60" height="60" src="http://localhost:8888/wp-content/plugins/tapestry/templates/img/TapestryLogo.png">
+            <img width="60" height="60" src="<?php echo plugin_dir_url(__FILE__); ?>img/TapestryLogo.png">
         </a>
         <div style="font-size:12px;margin: 20px 0 0 4px;white-space: nowrap;">Powered by Tapestry</div>
     </div>
-    <?php if(isset($_GET['show-open-link'])): ?>
+    <?php if (isset($_GET['show-open-link'])): ?>
     <a href="<?php echo strtok($_SERVER["REQUEST_URI"], '?'); ?>" target="_blank" style="color:#000;font-size:12px;margin: 20px 20px 0 4px;white-space: nowrap;">
         Open this tapestry in a new window 
         <i class="fas fa-external-link-alt" style="color:#186d95"></i>
