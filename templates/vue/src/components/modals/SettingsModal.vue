1f57ba75c64bbce437209eded8460160276938ef
Legendword <zwh@legendword.com>
2023-01-04 13:21:35 -0800

    Add setting to disable/enable dragging nodes for users without move permission

--- templates/vue/src/components/modals/SettingsModal.vue
+++ templates/vue/src/components/modals/SettingsModal.vue
@@ -105,6 +105,14 @@
             </b-form-checkbox>
           </b-form-group>
           <b-form-group
+            label="Allow moving all nodes"
+            description="When enabled, users can move all nodes even if they don't have permission to move them. Note that this only allows them to move the nodes locally; the movement will not be saved unless the user has move permission for that node."
+          >
+            <b-form-checkbox v-model="allowMovingAllNodes" switch>
+              {{ allowMovingAllNodes ? "Enabled" : "Disabled" }}
+            </b-form-checkbox>
+          </b-form-group>
+          <b-form-group
             label="Geography map"
             :description="
               'Replace Tapestry with a map of Earth with placeholders for each node.' +
@@ -300,6 +308,7 @@ export default {
       analyticsEnabled: false,
       draftNodesEnabled: true,
       submitNodesEnabled: true,
+      allowMovingAllNodes: false,
       renderMap: false,
       mapBounds: { neLat: 90, neLng: 180, swLat: -90, swLng: -180 },
     }
@@ -372,6 +381,7 @@ export default {
         analyticsEnabled = false,
         draftNodesEnabled = true,
         submitNodesEnabled = true,
+        allowMovingAllNodes = false,
         mapBounds = { neLat: 90, neLng: 180, swLat: -90, swLng: -180 },
       } = this.settings
       this.backgroundUrl = backgroundUrl
@@ -388,6 +398,7 @@ export default {
       this.analyticsEnabled = analyticsEnabled
       this.draftNodesEnabled = draftNodesEnabled
       this.submitNodesEnabled = submitNodesEnabled
+      this.allowMovingAllNodes = allowMovingAllNodes
       this.mapBounds = mapBounds
     },
     async updateSettings() {
@@ -406,6 +417,7 @@ export default {
         analyticsEnabled: this.analyticsEnabled,
         draftNodesEnabled: this.draftNodesEnabled,
         submitNodesEnabled: this.submitNodesEnabled,
+        allowMovingAllNodes: this.allowMovingAllNodes,
         mapBounds: this.mapBounds,
       })
       await this.$store.dispatch("updateSettings", settings)
